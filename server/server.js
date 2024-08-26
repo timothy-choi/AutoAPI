f269393e1659e6aca0b4c2bad5909b4d9c98d190
timothy-choi <tchoi720@gmail.com>
2024-08-25 20:41:17 -0700

    adding some more packages for server side work

--- server/server.js
+++ server/server.js
@@ -1,5 +1,19 @@
 const express = require('express.js');
+const dotenv = require('dotenv');
+const session = require('express-session');
 const app = express();
+
+dotenv.config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitalized: true,
+    cookie: {secure: false }
+}));
 
 
 const PORT = process.env.PORT || 3000;
