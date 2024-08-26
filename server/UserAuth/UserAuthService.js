ebee4028adb2bae839a325a2c7f488b5d460b6af
timothy-choi <tchoi720@gmail.com>
2024-08-26 15:51:19 -0700

    adding 2 functions for user auth service

---
+++ server/UserAuth/UserAuthService.js
@@ -0,0 +1,34 @@
+const { User } = require('../models'); // Import User model from Sequelize setup
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+exports.register = async (userData) => {
+    const username = userData.username;
+
+    if (username.length < 5) {
+        throw new Error('Username too short');
+    }
+
+    if (!/^[a-zA-Z0-9_]+$/.test(username)) {
+        throw new Error('Username must be 3-20 characters long and contain only letters, numbers, and underscores.');
+    }
+
+    const existingUser = await User.findOne({ where: { username } });
+    if (existingUser) {
+        throw new Error('Username is already taken.');
+    }
+
+    const hashedPassword = await bcrypt.hash(userData.password, 10);
+
+    const user = await User.create({username: userData.username, password: hashedPassword });
+    return user;
+}
+
+exports.login = async (credentials) => {
+    const user = await User.findOne({ where: { username: credentials.username } });
+    if (!user || !(await bcrypt.compare(credentials.password, user.password))) {
+        throw new Error("Invalid Credentials");
+    }
+
+    return jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '24h' });
+}
