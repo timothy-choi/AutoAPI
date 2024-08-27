b89d68ffa1f2803406180da97e89987f83e09d1d
timothy-choi <tchoi720@gmail.com>
2024-08-27 00:29:10 -0700

    changing outputs for controllers

--- server/UserAuth/UserAuthController.js
+++ server/UserAuth/UserAuthController.js
@@ -3,7 +3,7 @@ const userService = require('./UserAuthService');
 exports.register = async (req, res) => {
     try {
         await userService.register(req.body);
-        res.redirect("/login");
+        return res.status(201).json({msg: 'user registered'});
     } catch (error) {
         return res.status(400).json({ error: error.message });
     }
@@ -21,10 +21,10 @@ exports.login = async (req, res) => {
 exports.logout = async (req, res) => {
     req.session.destroy(err => {
         if (err) {
-            return res.status(500).send('Failed to logout');
+            return res.status(500).json({error: 'Failed to logout'});
         }
         res.clearCookie('connect.sid'); 
-        res.redirect('/login'); 
+        return res.status(201).json({msg: 'user logged out'}); 
     });
 }
 
@@ -43,10 +43,10 @@ exports.deleteUser = async (req, res) => {
 
         req.session.destroy(err => {
             if (err) {
-                return res.status(500).send('Failed to logout');
+                return res.status(500).json({error: 'Failed to logout'});
             }
             res.clearCookie('connect.sid'); 
-            return res.status(200).body({msg: 'account deleted successfully'}); 
+            return res.status(200).json({msg: 'account deleted successfully'}); 
         });
     } catch (error) {
         return res.status(500).json({error: 'Internal Server Error'});
