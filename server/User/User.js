f3d5fd54a9920d01f25ce28ceb67028eb692ce44
timothy-choi <tchoi720@gmail.com>
2024-08-27 15:09:33 -0700

    starting 1st draft of user model

---
+++ server/User/User.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.UUID,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowNull: false
+    },
+});
+
+module.exports = User;
