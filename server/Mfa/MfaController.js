05504c8d0c73598cd85e7105cea05561e9378930
timothy-choi <tchoi720@gmail.com>
2024-08-28 17:42:21 -0700

    added enable mfa implementation here

--- server/Mfa/MfaController.js
+++ server/Mfa/MfaController.js
@@ -1,6 +1,7 @@
 const speakeasy = require('speakeasy');
 const jwt = require('jsonwebtoken');
 const UserAuth = require('../UserAuth/UserAuth');
+const qrcode = require('qrcode');
 
 exports.verifyMFA = async (req, res) => {
     try {
@@ -21,6 +22,39 @@ exports.verifyMFA = async (req, res) => {
         req.session.token = newToken;
 
         return res.status(200).json({token: newToken});
+
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+};
+
+exports.enableMFA = async (req, res) => {
+    try {
+        const userAuth = await UserAuth.findOne({ where: { username: req.username } });
+
+        if (!userAuth) {
+            return res.status(404).json({error: 'User not found'});
+        }
+
+        const mfaSecret = speakeasy.generateSecret({ length: 20 });
+        userAuth.mfaSecret = mfaSecret.base32;
+        userAuth.mfaEnabled = true; 
+
+        await userAuth.save();
+
+        const otpAuthUrl = speakeasy.otpAuthUrl({
+            secret: user.mfaSecret,
+            label: `Auto API (${req.username})`,
+            issuer: 'Auto API'
+        });
+
+        qrcode.toDataURL(otpAuthUrl, (err, data_url) => {
+            if (err) {
+                throw new Error("Failed to generated QR code");
+            }
+
+            return res.status(201).json({msg: 'user mfa enabled', mfaSetupUrl: data_url});
+        });
 
     } catch (error) {
         return res.status(500).json({ error: error.message });
