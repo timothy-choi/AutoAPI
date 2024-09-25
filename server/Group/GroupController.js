77bf868a29ba35b3a4a0887cd672f961db27f628
timothy-choi <tchoi720@gmail.com>
2024-09-25 15:33:49 -0700

    adding create and delete endpoints for group

--- server/Group/GroupController.js
+++ server/Group/GroupController.js
@@ -20,5 +20,24 @@ exports.GetGroupByGroupName = async (req, res) => {
     }
 }
 
+exports.CreateGroup = async (req, res) => {
+    try {
+        var group = await GroupService.CreateGroup(req.body);
+        
+        return res.status(201).json(group);
+    } catch (error) {
+        return res.status(500).json({error: error.message});
+    }
+}
+
+exports.DeleteGroup = async (req, res) => {
+    try {
+        await GroupService.DeleteGroup(req.groupId);
+        
+        return res.status(201).json(null);
+    } catch (error) {
+        return res.status(500).json({error: error.message});
+    }
+}
 
 
--- server/Group/GroupRouter.js
+++ server/Group/GroupRouter.js
@@ -6,4 +6,8 @@ router.get("/:groupId", GroupController.GetGroupById);
 
 router.get("/groupName/:groupName", GroupController.GetGroupByGroupName);
 
+router.post("/", GroupController.CreateGroup);
+
+router.delete("/:groupId", GroupController.DeleteGroup);
+
 module.exports = router;
