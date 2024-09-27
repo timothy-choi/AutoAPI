0ef6dd8a63c07d1d972f0ace2ca441e8796f9929
timothy-choi <tchoi720@gmail.com>
2024-09-27 16:03:48 -0700

    added rest of endpoints

--- server/Messaging/MessagingController.js
+++ server/Messaging/MessagingController.js
@@ -30,6 +30,66 @@ exports.CreateMessaging = async (req, res) => {
     }
 }
 
+exports.AddUser = async (req, res) => {
+    try {
+        await MessagingService.AddUser(req.messagingId, req.user);
+
+        return res.status(200).json(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+exports.RemoveUser = async (req, res) => {
+    try {
+        await MessagingService.DeleteUser(req.messagingId, req.user);
+
+        return res.status(200).json(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+exports.AddSession = async (req, res) => {
+    try {
+        await MessagingService.AddSession(req.messagingId, req.sessionId);
+
+        return res.status(200).json(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+exports.RemoveSession = async (req, res) => {
+    try {
+        await MessagingService.DeleteSession(req.messagingId, req.sessionId);
+
+        return res.status(200).json(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+exports.AddMessage = async (req, res) => {
+    try {
+        await MessagingService.AddMessage(req.messagingId, req.messageId);
+
+        return res.status(200).json(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+exports.RemoveMessage = async (req, res) => {
+    try {
+        await MessagingService.DeleteMessage(req.messagingId, req.messageId);
+
+        return res.status(200).json(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
 exports.DeleteMessaging = async (req, res) => {
     try {
         await MessagingService.DeleteMessaging(req.messagingId);
