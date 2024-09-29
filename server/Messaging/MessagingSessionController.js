fe295f79df52335c0acd0659c30c0f35faf827f2
timothy-choi <tchoi720@gmail.com>
2024-09-28 21:22:33 -0700

    adding rest of endpoints

--- server/Messaging/MessagingSessionController.js
+++ server/Messaging/MessagingSessionController.js
@@ -30,6 +30,66 @@ exports.CreateMessagingSession = async (req, res) => {
     }
 }
 
+exports.SetUsername = async (req, res) => {
+    try {
+        await MessagingSessionService.EditUsername(req.messagingSessionId, req.user);
+
+        return res.status(200).json(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+exports.SetLastMessageReadId = async (req, res) => {
+    try {
+        await MessagingSessionService.SetLastMessageReadId(req.messagingSessionId, req.messageId);
+
+        return res.status(200).json(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+exports.SetLastActiveAt = async (req, res) => {
+    try {
+        await MessagingSessionService.SetLastActiveAt(req.messagingSessionId);
+
+        return res.status(200).json(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+exports.SetJoinedAt = async (req, res) => {
+    try {
+        await MessagingSessionService.SetJoinedAt(req.messagingSessionId);
+
+        return res.status(200).json(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+exports.SetClosedChatAt = async (req, res) => {
+    try {
+        await MessagingSessionService.SetClosedChatAt(req.messagingSessionId);
+
+        return res.status(200).json(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+exports.SetSessionStatus = async (req, res) => {
+    try {
+        await MessagingSessionService.SetSessionStatus(req.messagingSessionId, req.sessionStatus);
+
+        return res.status(200).json(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
 exports.DeleteMessagingSession = async (req, res) => {
     try {
         await MessagingSessionService.DeleteMessagingSession(req.messagingSessionId);
