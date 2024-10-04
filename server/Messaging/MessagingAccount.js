7476098d882aa971551934f83301ef621f2a4ad4
timothy-choi <tchoi720@gmail.com>
2024-10-03 22:14:12 -0700

    adding a model for user to keep track of all chats

---
+++ server/Messaging/MessagingAccount.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const MessagingAccount = sequelize.define('MessagingAccount', {
+    Id: {
+        type: DataTypes.UUID,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    UserId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+    },
+    Username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    AllChatroomsJoined: {
+        type: DataTypes.ARRAY(DataTypes.JSONB),
+        allowNull: true,
+        defaultValue: []
+    }
+});
+
+module.exports = MessagingAccount;
