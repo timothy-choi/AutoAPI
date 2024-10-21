2eb6f451ea2d1e57f61c9204887e5c29abf2dde9
timothy-choi <tchoi720@gmail.com>
2024-10-20 21:34:50 -0700

    adding create and delete project stats service functions

--- server/ApiGenerator/ProjectStatsService.js
+++ server/ApiGenerator/ProjectStatsService.js
@@ -10,4 +10,34 @@ exports.GetProjectStatsByProjectId = async (projectId) => {
     var projectStats = await ProjectStats.findOne({ where: {ProjectId: projectId }});
 
     return projectStats;
-}                                                                                                            
+}             
+
+exports.CreateProjectStats = async (projectStatsInfo) => {
+    try {
+        var currProjectStats = await ProjectStats.findOne({ where: {ProjectId: projectStatsInfo['projectId'] }});
+
+        if (currProjectStats) {
+            throw new Error('group already exists');
+        }
+
+        const projectStats = await ProjectStats.create({ProjectId: projectStatsInfo['projectId']});
+
+        return projectStats;
+    } catch (error) {
+        throw new Error('Could not create group');
+    }
+}
+
+exports.DeleteProjectStats = async (projectStatsId) => {
+    try {
+        var projectStats = await ProjectStats.findByPk(projectStatsId);
+
+        if (!projectStats) {
+            throw new Error('project stats does not exist');
+        }
+
+        await ProjectStats.destroy();
+    } catch (error) {
+        throw new Error('could not delete project stats');
+    }
+}
