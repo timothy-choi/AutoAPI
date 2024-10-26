df0f6dc0269114c3c1521f1a893487d9187f769a
Timothy Choi <tchoi720@gmail.com>
2024-10-25 20:44:11 -0700

    adding first attributres about postman api info and env info

--- server/ApiGenerator/ApiTesting.js
+++ server/ApiGenerator/ApiTesting.js
@@ -1,5 +1,43 @@
 const mongoose = require('mongoose');
 
+const PostmanApiInfoSchema = mongoose.Schema({
+    TestingId: {
+        type: String,
+        required: true
+    },
+    Name: {
+        type: String,
+        required: true
+    },
+    BaseUrl: {
+        type: String,
+        required: false
+    },
+    MainCollectionId: {
+        type: String,
+        required: true
+    }
+});
+
+const PostmanApiEnvironmentSchema = mongoose.Schema({
+    TestingId: {
+        type: String,
+        required: true
+    },
+    ProjectId: {
+        type: String,
+        required: true
+    },
+    Name: {
+        type: String,
+        required: true
+    },
+    Variables: {
+        type: [mongoose.Schema.Types.Mixed],
+        required: true
+    }
+})
+
 const ApiTestingSchema = mongoose.Schema({
     ProjectId: {
         type: String,
@@ -14,6 +52,14 @@ const ApiTestingSchema = mongoose.Schema({
         type: [mongoose.Schema.Types.Mixed],
         required: true
     },
+    PostmanApiInfo: {
+        type: PostmanApiInfoSchema,
+        required: true
+    },
+    PostmanEnvironmentInfo: {
+        type: PostmanApiEnvironmentSchema,
+        required: true
+    }
 });
 
 const ApiTesting = mongoose.model('ApiTesting', ApiTestingSchema);
