2d4801201084576481dc2d64c053fb5453d6dc41
Timothy Choi <tchoi720@gmail.com>
2024-10-26 19:04:58 -0700

    adding create and delete service functions for database model

--- server/ApiGenerator/DatabaseService.js
+++ server/ApiGenerator/DatabaseService.js
@@ -13,3 +13,57 @@ exports.getDatabaseByName = async (databaseName) => {
 
     return database;
 };
+
+exports.CreateDatabase = async (databaseInfo) => {
+    var database = await DatabaseDefinition.findOne({Name: databaseInfo.Name});
+
+    if (database) {
+        throw new Exception('Database already exists');
+    }
+
+    database = await DatabaseDefinition.Create({Name:  databaseInfo.Name, 
+                                                Type: databaseInfo.Type, 
+                                                Version: databaseInfo.Version, 
+                                                Description: databaseInfo.Description,
+                                                CreatedAt: Date.now(),
+                                                CreatedBy: databaseInfo.CreatedBy,
+                                                ModelsUsed: databaseInfo.ModelsUsed });
+    
+    return database;
+}
+
+exports.EditDescription = async (databaseId, desc, username) => {
+    try {
+        var database = await this.getDatabaseById(databaseId);
+
+        if (!database) {
+            throw new Exception('database does not exist');
+        }
+
+        database.Description = desc;
+
+        database.DidUpdate = true;
+
+        database.UpdatedAt = Date.now();
+
+        database.UpdatedBy = username;
+
+        await database.save();
+    } catch (error) {
+        throw new Exception('Can not edit database');
+    }
+}
+
+exports.DeleteDatabase = async (databaseId) => {
+    try {
+        var database = await this.getDatabaseById(databaseId);
+
+        if (!database) {
+            throw new Exception('database does not exist');
+        }
+
+        await database.destroy();
+    } catch (error) {
+        throw new Exception('Can not delete database');
+    }
+}
