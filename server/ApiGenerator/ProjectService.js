86e6a39cd251d86fbe8e6b81d1b94f35db612eca
Timothy Choi <tchoi720@gmail.com>
2024-11-22 22:13:08 -0800

    adding model service functions for project model

--- server/ApiGenerator/ProjectService.js
+++ server/ApiGenerator/ProjectService.js
@@ -11,3 +11,61 @@ exports.GetProjectByName = async (projectName) => {
 
     return project;
 }
+
+exports.AddModelToProject = async (projectId, modelInfo) => {
+    try {
+        var project = await Project.findByPk(projectId);
+
+        if (!project) {
+            throw new Exception('Can not get project');
+        }
+
+        project.AllProjectModels.push(modelInfo);
+
+        project.ModifiedAt = Date.now;
+
+        await project.save();
+    } catch (error) {
+        throw new Exception('Can not edit project');
+    }
+}
+
+exports.RemoveModelToProject = async (projectId, modelInfoId) => {
+    try {
+        var project = await GetProjectById(projectId);
+
+        if (!project) {
+            throw new Error('Project does not exist');
+        } 
+
+        project.AllProjectModels.filter(modelInfo => modelInfo.id != modelInfoId);
+
+        project.ModifiedAt = Date.now();
+
+        await project.save();
+    } catch (error) {
+        throw new Exception('Can not edit project');
+    }
+}
+
+exports.EditModelToProject = async (projectId, modelInfoId, updatedModelInfo) => {
+    try {
+        var project = await GetProjectById(projectId);
+
+        if (!project) {
+            throw new Error('Model does not exist');
+        } 
+
+        var index = project.AllProjectModels.findIndex(modelInfo = modelInfo.id != modelInfoId);
+
+        project.AllProjectModels.splice(index, 1);
+
+        project.AllProjectModels.splice(index, 0, updatedModelInfo);
+
+        project.ModifiedAt = Date.now();
+
+        await project.save();
+    } catch (error) {
+        throw new Error('could not delete model');
+    }
+}
