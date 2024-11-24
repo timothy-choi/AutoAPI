08277246d0f7dc3153269a419ccf76671182efbb
Timothy Choi <tchoi720@gmail.com>
2024-11-24 14:48:01 -0800

    adding rest of database endpoints

--- server/ApiGenerator/DatabaseController.js
+++ server/ApiGenerator/DatabaseController.js
@@ -90,6 +90,116 @@ exports.editModelTableInfo = async (req, res) => {
     }
 }
 
+exports.modifyDatabaseInstanceInfo = async (req, res) => {
+    try {
+        await DatabaseService.ModifyDatabaseInstanceInfo(req.databaseId, req.body, req.username);
+
+        return res.status(200).json(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+exports.setHealthStatus = async (req, res) => {
+    try {
+        await DatabaseService.SetHealthStatus(req.databaseId, req.healthStatus, req.username);
+
+        return res.status(200).json(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+exports.setStatus = async (req, res) => {
+    try {
+        await DatabaseService.SetStatus(req.databaseId, req.databaseStatus, req.username);
+
+        return res.status(200).json(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+exports.addDatabaseChangeLog = async (req, res) => {
+    try {
+        await DatabaseService.AddDatabaseChangeLog(req.databaseId, req.body, req.username);
+
+        return res.status(200).json(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+exports.addDatabaseOperationsLog = async (req, res) => {
+    try {
+        await DatabaseService.AddDatabaseOperationsLog(req.databaseId, req.body, req.username);
+
+        return res.status(200).json(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+exports.modifyDatabaseBackupInfo = async (req, res) => {
+    try {
+        await DatabaseService.ModifyDatabaseBackupInfo(req.databaseId, req.body, req.username);
+
+        return res.status(200).json(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+exports.addDatabaseVersionHistory = async (req, res) => {
+    try {
+        await DatabaseService.AddDatabaseVersionHistory(req.databaseId, req.body, req.username);
+
+        return res.status(200).json(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+exports.modifyDatabaseCloudInfo = async (req, res) => {
+    try {
+        await DatabaseService.ModifyDatabaseCloudInfo(req.databaseId, req.body, req.username);
+
+        return res.status(200).json(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+exports.addServerlessFunction = async (req, res) => {
+    try {
+        await DatabaseService.AddServerlessFunction(req.databaseId, req.body, req.username);
+
+        return res.status(200).json(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+exports.removeServerlessFunction = async (req, res) => {
+    try {
+        await DatabaseService.RemoveServerlessFunction(req.databaseId, req.body, req.username);
+
+        return res.status(200).json(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+exports.editServerlessFunction = async (req, res) => {
+    try {
+        await DatabaseService.editServerlessFunction(req.databaseId, req.serverlessFunctionId, req.body, req.username);
+
+        return res.status(200).json(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
 exports.deleteDatabase = async (req, res) => {
     try {
         await DatabaseService.DeleteDatabase(req.databaseId);
