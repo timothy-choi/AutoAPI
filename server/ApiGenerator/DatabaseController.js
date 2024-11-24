d10cda225161e34a5781bc4499a81ca62ddd3b23
Timothy Choi <tchoi720@gmail.com>
2024-11-24 13:48:46 -0800

    added more database endpoints

--- server/ApiGenerator/DatabaseController.js
+++ server/ApiGenerator/DatabaseController.js
@@ -30,6 +30,66 @@ exports.createDatabase = async (req, res) => {
     }
 }
 
+exports.editDatabaseDescription = async (req, res) => {
+    try {
+        await DatabaseService.EditDescription(req.databaseId, req.body.desc, req.username);
+
+        return res.status(200).json(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+exports.addModelsUsed = async (req, res) => {
+    try {
+        await DatabaseService.addModelsUsed(req.databaseId, req.model, req.username);
+
+        return res.status(200).json(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+exports.removeModelsUsed = async (req, res) => {
+    try {
+        await DatabaseService.removeModelsUsed(req.databaseId, req.model, req.username);
+
+        return res.status(200).json(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+exports.addModelTableInfo = async (req, res) => {
+    try {
+        await DatabaseService.addModelTableInfo(req.databaseId, req.body, req.username);
+
+        return res.status(200).json(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+exports.removeModelTableInfo = async (req, res) => {
+    try {
+        await DatabaseService.removeModelTableInfoUsed(req.databaseId, req.body, req.username);
+
+        return res.status(200).json(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+exports.editModelTableInfo = async (req, res) => {
+    try {
+        await DatabaseService.editModelTableInfo(req.databaseId, req.modelTableInfoId, req.body, req.username);
+
+        return res.status(200).json(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
 exports.deleteDatabase = async (req, res) => {
     try {
         await DatabaseService.DeleteDatabase(req.databaseId);
