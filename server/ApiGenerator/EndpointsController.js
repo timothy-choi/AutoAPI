5ed08442c631b71c94c25d9ec67489e08cd0e07f
Timothy Choi <tchoi720@gmail.com>
2024-11-25 14:16:48 -0800

    adding endpoint model and db endpoints

--- server/ApiGenerator/EndpointsController.js
+++ server/ApiGenerator/EndpointsController.js
@@ -60,6 +60,66 @@ exports.EditEndpointHeader = async (req, res) => {
     }
 }
 
+exports.AddEndpointModel = async (req, res) => {
+    try {
+        await EndpointService.AddEndpointModel(req.endpointId, req.body, req.username);
+
+        return res.status(200).body(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+exports.RemoveEndpointModel = async (req, res) => {
+    try {
+        await EndpointService.RemoveEndpointModel(req.endpointId, req.endpointModelId, req.username);
+
+        return res.status(200).body(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+exports.EditEndpointModel = async (req, res) => {
+    try {
+        await EndpointService.EditEndpointModel(req.endpointId, req.endpointModelId, req.body, req.username);
+
+        return res.status(200).body(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+exports.AddEndpointDatabase = async (req, res) => {
+    try {
+        await EndpointService.AddEndpointDatabase(req.endpointId, req.body, req.username);
+
+        return res.status(200).body(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+exports.RemoveEndpointDatabase = async (req, res) => {
+    try {
+        await EndpointService.RemoveEndpointDatabase(req.endpointId, req.endpointDatabaseId, req.username);
+
+        return res.status(200).body(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+exports.EditEndpointDatabase = async (req, res) => {
+    try {
+        await EndpointService.EditEndpointDatabase(req.endpointId, req.endpointDatabaseId, req.body, req.username);
+
+        return res.status(200).body(null);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
 exports.DeleteEndpoints = async (req, res) => {
     try {
         await EndpointService.DeleteEndpoints(req.endpointId);
