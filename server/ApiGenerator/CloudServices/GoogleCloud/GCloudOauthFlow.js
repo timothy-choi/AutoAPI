4d1536898fa749779c54d62319c69eab0266b995
Timothy Choi <tchoi720@gmail.com>
2024-12-04 14:20:40 -0800

    adding callback endpoint and refresh token helper func for gcloud but may need to make some big changes to it

--- server/ApiGenerator/CloudServices/GoogleCloud/GCloudOauthFlow.js
+++ server/ApiGenerator/CloudServices/GoogleCloud/GCloudOauthFlow.js
@@ -18,8 +18,40 @@ exports.LoginToGCloud = async (req, res) => {
         access_type: 'offline', 
         scope: GCLOUD_SCOPES,
     });
-    
+
     res.redirect(url);
 }
 
+exports.GCloudOAuthCallback = async (req, res) => {
+    const code = req.query.code;
+    if (!code) {
+        return res.status(400).send('No code returned from Google');
+    }
 
+    try {
+        const { tokens } = await oauth2Client.getToken(code);
+        oauth2Client.setCredentials(tokens);
+
+        return res.status(200).send({"accessToken": tokens.access_token, "refreshToken": tokens.refresh_token});
+    } catch (error) {
+        return res.status(500).send("Failed to authenticate");
+    }
+}
+
+exports.RefreshAccessToken = async (refreshToken) => {
+    oauth2Client.setCredentials({ refresh_token: refreshToken });
+
+    try {
+        const { token: newAccessToken, res } = await oauth2Client.getAccessToken();
+
+        const newRefreshToken = res.data.refresh_token || refreshToken;
+
+        return {
+            accessToken: newAccessToken,
+            refreshToken: newRefreshToken,
+        };
+    } catch (error) {
+        console.error('Error refreshing access token:', error);
+        throw error;
+    }
+}
