66e9a1f67911a684f378262e52fd6a2541523742
Timothy Choi <tchoi720@gmail.com>
2024-12-03 21:36:55 -0800

    added impl of aws oauth callback

--- server/ApiGenerator/CloudServices/AWS/AwsOauthFlow.js
+++ server/ApiGenerator/CloudServices/AWS/AwsOauthFlow.js
@@ -1,20 +1,43 @@
 require("dotenv").config();
 
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = process.env.REDIRECT_URI;
-const AUTH_URL = process.env.AUTH_URL;
-const TOKEN_URL = process.env.TOKEN_URL;
+const AWS_CLIENT_ID = process.env.CLIENT_ID;
+const AWS_CLIENT_SECRET = process.env.CLIENT_SECRET;
+const AWS_REDIRECT_URI = process.env.REDIRECT_URI;
+const AWS_AUTH_URL = process.env.AUTH_URL;
+const AWS_TOKEN_URL = process.env.TOKEN_URL;
 
 const axios = require('axios');
 
 exports.LoginToAWS = async (req, res) => {
-    const authorizationUrl = `${AUTH_URL}?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(
-        REDIRECT_URI
+    const authorizationUrl = `${AWS_AUTH_URL}?response_type=code&client_id=${AWS_CLIENT_ID}&redirect_uri=${encodeURIComponent(
+        AWS_REDIRECT_URI
       )}&scope=openid email profile`;
     
     res.redirect(authorizationUrl);
 }
 
+exports.GetTokenFromAWS = async (req, res) => {
+    const authorizationCode = req.query.code;
 
+    if (!authorizationCode) {
+        return res.status(400).send("Authorization code not found");
+    }
 
+    try {
+        const tokenResponse = await axios.post(
+            AWS_TOKEN_URL,
+            new URLSearchParams({
+              grant_type: "authorization_code",
+              code: authorizationCode,
+              redirect_uri: AWS_REDIRECT_URI,
+              client_id: AWS_CLIENT_ID,
+              client_secret: AWS_CLIENT_SECRET,
+            }),
+            { headers: { "Content-Type": "application/x-www-form-urlencoded" } }
+        );
+
+        return res.status(200).send({"AccessToken": tokenResponse.data.access_token, "RefreshToken": tokenResponse.data.refresh_token});
+    } catch (error) {
+        return res.status(500).send("Failed to authenticate");
+      }
+}
