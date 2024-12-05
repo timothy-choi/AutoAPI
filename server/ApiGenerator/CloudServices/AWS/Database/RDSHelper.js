684c61843252c1990d9ae6d3d135cd204bfd41e5
Timothy Choi <tchoi720@gmail.com>
2024-12-05 00:01:30 -0800

    adding starting and stopping rds instances helper functions for aws

--- server/ApiGenerator/CloudServices/AWS/Database/RDSHelper.js
+++ server/ApiGenerator/CloudServices/AWS/Database/RDSHelper.js
@@ -22,6 +22,33 @@ exports.describeRDSInstance = async (currDbId, userCredentials, userRegion) => {
     return data.DBInstances[0];
 }
 
+exports.startRDSInstance = async (currDbId, userCredentials, userRegion) => {
+    const rds = new AWS.RDS({
+        credentials: new AWS.Credentials(userCredentials.accessKey, userCredentials.userSecretKey, userCredentials.sessionToken),
+        region: userRegion
+    });
+
+    const params = {
+        DBInstanceIdentifier: currDbId
+    };
+
+    await rds.startDBInstance(params).promise();
+}
+
+exports.stopRDSInstance = async (currDbId, userCredentials, userRegion) => {
+    const rds = new AWS.RDS({
+        credentials: new AWS.Credentials(userCredentials.accessKey, userCredentials.userSecretKey, userCredentials.sessionToken),
+        region: userRegion
+    });
+
+    const params = {
+        DBInstanceIdentifier: currDbId
+    };
+
+    await rds.stopDBInstance(params).promise();
+}
+
+
 exports.deleteRDSInstance = async (dbId, skipFinalSnapshot = true, userCredentials, userRegion) => {
     const rds = new AWS.RDS({
         credentials: new AWS.Credentials(userCredentials.accessKey, userCredentials.userSecretKey, userCredentials.sessionToken),
