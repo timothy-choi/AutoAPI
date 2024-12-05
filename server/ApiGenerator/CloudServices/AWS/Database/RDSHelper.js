0f60b0249da4070a9acbae5f8d98ac03aabbb8c7
Timothy Choi <tchoi720@gmail.com>
2024-12-05 11:41:52 -0800

    adding mechanisms to waiting for rds instances to be created, deleted, started, and stopped successfully

--- server/ApiGenerator/CloudServices/AWS/Database/RDSHelper.js
+++ server/ApiGenerator/CloudServices/AWS/Database/RDSHelper.js
@@ -6,9 +6,20 @@ exports.createRDSInstance = async (dbInstanceInfo, userCredentials, userRegion) 
         region: userRegion
     });
 
-    const data = await rds.createDBInstance(dbInstanceInfo).promise();
+    const instanceData = await rds.createDBInstance(dbInstanceInfo).promise();
 
-    return data;
+    let status = "creating";
+    while (status === "creating") {
+        await new Promise((resolve) => setTimeout(resolve, 15000));
+        const data = await rds.describeDBInstances({ DBInstanceIdentifier:  dbInstanceInfo.DBInstanceIdentifier}).promise();
+        status = data.DBInstances[0].DBInstanceStatus;
+    }
+
+    if (status === "available") {
+        return instanceData;
+    } else {
+        throw new Error(`Instance creation failed with status: ${status}`);
+    }
 }
 
 exports.describeRDSInstance = async (currDbId, userCredentials, userRegion) => {
@@ -32,7 +43,21 @@ exports.startRDSInstance = async (currDbId, userCredentials, userRegion) => {
         DBInstanceIdentifier: currDbId
     };
 
-    await rds.startDBInstance(params).promise();
+    try {
+        await rds.startDBInstance(params).promise();
+
+        let instanceStatus = "";
+        do {
+            const data = await rds.describeDBInstances({ DBInstanceIdentifier: dbId }).promise();
+            instanceStatus = data.DBInstances[0].DBInstanceStatus;
+            if (instanceStatus !== "available") {
+                await new Promise((resolve) => setTimeout(resolve, 15000)); 
+            }
+        } while (instanceStatus !== "available");
+
+    } catch (error) {
+        throw new Error('RDS instance failed to start', error);
+    }
 }
 
 exports.stopRDSInstance = async (currDbId, userCredentials, userRegion) => {
@@ -45,7 +70,21 @@ exports.stopRDSInstance = async (currDbId, userCredentials, userRegion) => {
         DBInstanceIdentifier: currDbId
     };
 
-    await rds.stopDBInstance(params).promise();
+    try {
+        await rds.stopDBInstance(params).promise();
+
+        let instanceStatus = "";
+        do {
+            const data = await rds.describeDBInstances({ DBInstanceIdentifier: dbId }).promise();
+            instanceStatus = data.DBInstances[0].DBInstanceStatus;
+            if (instanceStatus !== "stopped") {
+                await new Promise((resolve) => setTimeout(resolve, 15000)); 
+            }
+        } while (instanceStatus !== "stopped");
+
+    } catch (error) {
+        throw new Error('RDS instance failed to stop', error);
+    }
 }
 
 exports.modifyRDSInstance = async (changedDbAttributes, userCredentials, userRegion) => {
@@ -117,4 +156,18 @@ exports.deleteRDSInstance = async (dbId, skipFinalSnapshot = true, userCredentia
     }
 
     await rds.deleteDBInstance(params).promise();
+
+    let isDeleted = false;
+    while (!isDeleted) {
+        try {
+            await new Promise((resolve) => setTimeout(resolve, 15000)); 
+            const data = await rds.describeDBInstances({ DBInstanceIdentifier: instanceId }).promise();
+        } catch (error) {
+            if (error.code === "DBInstanceNotFound") {
+                isDeleted = true;
+            } else {
+                throw new Error(error.Message);
+            }
+        }
+    }
 }
