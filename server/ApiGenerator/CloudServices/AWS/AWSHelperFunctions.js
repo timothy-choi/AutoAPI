e322dfd06643288196e941247c5c387a3f501a32
Timothy Choi <tchoi720@gmail.com>
2024-12-04 17:25:50 -0800

    implemented func to create a vpc in aws

---
+++ server/ApiGenerator/CloudServices/AWS/AWSHelperFunctions.js
@@ -0,0 +1,31 @@
+const AWS = require('aws-sdk');
+
+const ec2 = new AWS.EC2();
+
+const createVPC = async (cidrBlock, vpcName) => {
+    const vpcResponse = await ec2.createVpc({CidrBlock: cidrBlock});
+
+    const vpcId = vpcResponse.Vpc.VpcId;
+
+    const tagParams = {
+        Resources: [vpcId],
+        Tags: [
+            {
+                Key: "Name",
+                Value: vpcName, 
+            },
+        ],
+    }
+
+    await ec2.createTags(tagParams).promise();
+
+    const modifyVpcParams = {
+        VpcId: vpcId,
+        EnableDnsSupport: { Value: true },
+        EnableDnsHostnames: { Value: true },
+    };
+
+    await ec2.modifyVpcAttribute(modifyVpcParams).promise();
+
+    return vpcId;
+}
