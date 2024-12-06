2f2c574b61d4a460b1ea05bc8127e3c606f8ed37
Timothy Choi <tchoi720@gmail.com>
2024-12-06 11:32:44 -0800

    added routes for db service

--- server/ApiGenerator/ProjectRouter.js
+++ server/ApiGenerator/ProjectRouter.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const ModelController = require('./ModelController');
+const DatabaseController = require('./DatabaseController');
 
 router.get("/Model/:modelId", ModelController.GetModelById);
 
-router.get("/Model/:modelName", ModelController.GetModelByName);
+router.get("/Model/name/:modelName", ModelController.GetModelByName);
 
-router.post("/Model/", ModelController.CreateModel);
+router.post("/Model", ModelController.CreateModel);
 
 router.put("/Model/:modelId/Version/:version", ModelController.EditVersion);
 
@@ -27,9 +28,51 @@ router.put("/Model/:modelId/ModelChangelogInfo", ModelController.AddModelChangel
 router.delete("/Model/:modelId", ModelController.DeleteModel);
 
 
+router.get("/Database/:databaseId", DatabaseController.getDatabaseById);
 
+router.get("/Database/name/:databaseName", DatabaseController.getDatabaseByName);
 
+router.post("/Database", DatabaseController.createDatabase);
 
+router.put("/Database/:databaseId/Description/:username", DatabaseController.editDatabaseDescription);
+
+router.put("/Database/:databaseId/ModelsUsed/add/:model/:username", DatabaseController.addModelsUsed);
+
+router.put("/Database/:databaseId/ModelsUsed/remove/:model/:username", DatabaseController.removeModelsUsed);
+
+router.put("/Database/:databaseId/ModelTableInfo/add/:username", DatabaseController.addModelTableInfo);
+
+router.put("/Database/:databaseId/ModelTableInfo/remove/:username", DatabaseController.removeModelTableInfo);
+
+router.put("/Database/:databaseId/ModelTableInfo/edit/:modelTableInfoId/:username", DatabaseController.editModelTableInfo);
+
+router.put("/Database/:databaseId/DatabaseInstanceInfo/:username", DatabaseController.modifyDatabaseInstanceInfo);
+
+router.put("/Database/:databaseId/HealthStatus/:healthStatus/:username", DatabaseController.setHealthStatus);
+
+router.put("/Database/:databaseId/Status/:status/:username", DatabaseController.setStatus);
+
+router.put("/Database/:databaseId/DatabaseChangeLog/:username", DatabaseController.addDatabaseChangeLog);
+
+router.put("/Database/:databaseId/DatabaseOperationsLog/:username", DatabaseController.addDatabaseOperationsLog)
+
+router.put("/Database/:databaseId/DatabaseBackupInfo/:username", DatabaseController.modifyDatabaseBackupInfo);
+
+router.put("/Database/:databaseId/DatabaseVersionHistory/:username", DatabaseController.addDatabaseVersionHistory);
+
+router.put("/Database/:databaseId/DatabaseCloudInfo/:username", DatabaseController.modifyDatabaseCloudInfo);
+
+router.put("/Database/:databaseId/ServerlessFunction/add/:username", DatabaseController.addServerlessFunction);
+
+router.put("/Database/:databaseId/ServerlessFunction/remove/:username", DatabaseController.removeServerlessFunction);
+
+router.put("/Database/:databaseId/ServerlessFunction/edit/:serverlessFunctionId/:username", DatabaseController.editServerlessFunction);
+
+router.put("/Database/:databaseId/DatabaseUsageInfo/:username", DatabaseController.editDatabaseUsageInfo);
+
+router.put("/Database/:databaseId/DatabaseBillingInfo/:username", DatabaseController.editDatabaseBillingInfo);
+
+router.delete("/Database/:databaseId", DatabaseController.deleteDatabase);
 
 
 module.exports = router;
