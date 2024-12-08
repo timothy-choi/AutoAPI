a83f74e1861b9e4dde57d6330f8a3487bf744479
Timothy Choi <tchoi720@gmail.com>
2024-12-07 23:58:27 -0800

    added a helper endpoint to create the rds instance

--- server/ApiGenerator/CloudServices/AWS/Database/RDSController.js
+++ server/ApiGenerator/CloudServices/AWS/Database/RDSController.js
@@ -3,12 +3,34 @@ const AWSHelper = require("../AWSHelperFunctions");
 
 exports.getRDSInstanceAvailability = async (req, res) => {
     try {
-        const userCredentials = await AWSHelper.getAWSCredentials(req.body.secretName);
+        let userCredentials = {};
+        if (req.body.userCredentialsInfo) {
+            userCredentials = req.body.userCredentialsInfo;
+        } else {
+            userCredentials = await AWSHelper.getAWSCredentials(req.body.secretName);
+        }
 
         var availability = await RDSHelper.checkRDSInstanceAvailability(req.currDbId, userCredentials, req.body.userRegion);
 
         return res.status(200).send({"availability": availability});
     } catch (error) {
         return res.status(500).send("Error getting RDS instance availability: " + error);
+    }
+}
+
+exports.createRDSInstance = async (req, res) => {
+    try {
+        let userCredentials = {};
+        if (req.body.userCredentialsInfo) {
+            userCredentials = req.body.userCredentialsInfo;
+        } else {
+            userCredentials = await AWSHelper.getAWSCredentials(req.body.secretName);
+        }
+
+        const rdsInstanceInfo = await RDSHelper.createRDSInstance(req.body.dbInstanceInfo, userCredentials, req.body.userRegion);
+
+        return res.status(201).json(rdsInstanceInfo);
+    } catch (error) {
+        return res.status(500).send("Error creating RDS instance: " + error);
     }
 }
