cb5b55b0eed05fe9d58a21a0520c3ffc287b495d
Timothy Choi <tchoi720@gmail.com>
2024-12-09 23:06:16 -0800

    added a few more operations to dynamodb helper

--- server/ApiGenerator/CloudServices/AWS/Database/DynamoDBHelper.js
+++ server/ApiGenerator/CloudServices/AWS/Database/DynamoDBHelper.js
@@ -12,3 +12,52 @@ exports.CreateDynamoDBTable = async (userCredentials, tableParams) => {
 
     return tableResponse;
 }
+
+exports.DescribeTable = async (userCredentials, tableName) => {
+    const dynamodb = new AWS.DynamoDB({
+        accessKeyId: userCredentials.accessKey,
+        secretAccessKey: userCredentials.secretKey,
+        sessionToken: userCredentials.sessionToken,
+        region: userCredentials.region
+    });
+
+    const params = {
+        TableName: tableName, 
+    };
+
+    const tableResponse = await dynamodb.createTable(params).promise();
+
+    return tableResponse;
+}
+
+exports.GetTableStatus = async (userCredentials, tableName) => {
+    const dynamodb = new AWS.DynamoDB({
+        accessKeyId: userCredentials.accessKey,
+        secretAccessKey: userCredentials.secretKey,
+        sessionToken: userCredentials.sessionToken,
+        region: userCredentials.region
+    });
+
+    const params = {
+        TableName: tableName, 
+    };
+
+    const tableResponse = await dynamodb.createTable(params).promise();
+
+    return tableResponse.TableDescription.TableStatus;
+}
+
+exports.DeleteDynamoDBTable = async (userCredentials, tableName) => {
+    const dynamodb = new AWS.DynamoDB({
+        accessKeyId: userCredentials.accessKey,
+        secretAccessKey: userCredentials.secretKey,
+        sessionToken: userCredentials.sessionToken,
+        region: userCredentials.region
+    });
+
+    const params = {
+        TableName: tableName, 
+    };
+
+    await dynamodb.deleteTable(params).promise();
+}
