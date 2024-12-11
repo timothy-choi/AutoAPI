b5437b981f21170621fb8fa476b23056eebd216a
Timothy Choi <tchoi720@gmail.com>
2024-12-10 17:16:57 -0800

    adding helper functions to manage data in mysql db

--- server/ApiGenerator/CloudServices/AWS/Database/RDSDatabases/MySQLHelper.js
+++ server/ApiGenerator/CloudServices/AWS/Database/RDSDatabases/MySQLHelper.js
@@ -12,6 +12,35 @@ exports.connectToMySQLDatabase = async (connectionInfo) => {
     }
 }
 
+exports.ExecuteQuery = async (query, params, poolInstance) => {
+    try {
+        const [results] = await poolInstance.query(query, params);
+        return results;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
+
+exports.InsertToMySQLDatabase = async (query, values, poolInstance) => {
+    try {
+        const [result] = await poolInstance.execute(query, values);
+
+        return result.insertId;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
+
+exports.RemoveOrModifyInMySQLDatabase = async (query, values, poolInstance) => {
+    try {
+        const [result] = await poolInstance.execute(query, values);
+
+        return result.affectedRows;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
+
 exports.endMySQLConnection = async (pool) => {
     try {
         await pool.end();
