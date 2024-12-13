b41cc20a711194092cede98f750d455573d8b5a2
Timothy Choi <tchoi720@gmail.com>
2024-12-13 15:39:40 -0800

    adding retry mechanism for dynamo db functions

--- server/ApiGenerator/CloudServices/AWS/Database/DynamoDBHelper.js
+++ server/ApiGenerator/CloudServices/AWS/Database/DynamoDBHelper.js
@@ -1,5 +1,24 @@
 const AWS = require('aws-sdk');
 
+const executeWithRetry = async (operation, maxRetries = 3, initialDelay = 100, backoffFactor = 2) => {
+    let attempt = 0;
+    let delay = initialDelay;
+
+    while (attempt <= maxRetries) {
+        try {
+            return await operation();
+        } catch (error) {
+            attempt++;
+            if (attempt > maxRetries) {
+                throw new Error(error.message);
+            }
+
+            await new Promise(resolve => setTimeout(resolve, delay));
+            delay *= backoffFactor;
+        }
+    }
+}
+
 exports.CreateDynamoDBTable = async (userCredentials, tableParams) => {
     try {
         const dynamodb = new AWS.DynamoDB({
@@ -8,10 +27,14 @@ exports.CreateDynamoDBTable = async (userCredentials, tableParams) => {
             sessionToken: userCredentials.sessionToken,
             region: userCredentials.region
         });
+
+        const operation = async () => {
+            const tableResponse = await dynamodb.createTable(tableParams).promise();
     
-        const tableResponse = await dynamodb.createTable(tableParams).promise();
-    
-        return tableResponse;
+            return tableResponse;
+        };
+
+        return executeWithRetry(operation, 3, 20, 2);
     } catch (error) {
         throw new Error(error.message);
     }
@@ -26,13 +49,17 @@ exports.DescribeTable = async (userCredentials, tableName) => {
             region: userCredentials.region
         });
 
-        const params = {
-            TableName: tableName, 
+        const operation = async () => {
+            const params = {
+                TableName: tableName, 
+            };
+
+            const tableResponse = await dynamodb.describeTable(params).promise();
+
+            return tableResponse;
         };
 
-        const tableResponse = await dynamodb.describeTable(params).promise();
-
-        return tableResponse;
+        return executeWithRetry(operation, 3, 20, 3);
     } catch (error) {
         throw new Error(error.message);
     }
@@ -47,13 +74,17 @@ exports.GetTableStatus = async (userCredentials, tableName) => {
             region: userCredentials.region
         });
 
-        const params = {
-            TableName: tableName, 
+        const operation = async () => {
+            const params = {
+                TableName: tableName, 
+            };
+
+            const tableResponse = await dynamodb.describeTable(params).promise();
+
+            return tableResponse.TableDescription.TableStatus;
         };
 
-        const tableResponse = await dynamodb.describeTable(params).promise();
-
-        return tableResponse.TableDescription.TableStatus;
+        return executeWithRetry(operation, 3, 20, 3);
     } catch (error) {
         throw new Error(error.message);
     }
@@ -67,10 +98,14 @@ exports.PutTableEntry = async (userCredentials, tableName, entryInfo) => {
             sessionToken: userCredentials.sessionToken,
             region: userCredentials.region
         });
+
+        const operation = async () => {
+            const params = { TableName: tableName, Item: entryInfo };
     
-        const params = { TableName: tableName, Item: entryInfo };
-    
-        await docClient.put(params).promise();
+            await docClient.put(params).promise();
+        }
+
+        executeWithRetry(operation, 3, 20, 3);
     } catch (error) {
         throw new Error(error.message);
     }
@@ -84,10 +119,14 @@ exports.DeleteTableEntry = async (userCredentials, tableName, keyValue) => {
             sessionToken: userCredentials.sessionToken,
             region: userCredentials.region
         });
-    
-        const params = { TableName: tableName, Key: keyValue };
-    
-        await docClient.delete(params).promise();
+
+        const operation = async () => {
+            const params = { TableName: tableName, Key: keyValue };
+        
+            await docClient.delete(params).promise();
+        }
+
+        executeWithRetry(operation, 3, 20, 3);
     } catch (error) {
         throw new Error(error.message);
     }
@@ -101,12 +140,16 @@ exports.GetTableEntry = async (userCredentials, tableName, keyValue) => {
             sessionToken: userCredentials.sessionToken,
             region: userCredentials.region
         });
-    
-        const params = { TableName: tableName, Key: keyValue };
-    
-        var data = await docClient.get(params).promise();
 
-        return data.Item;
+        const operation = async () => {
+            const params = { TableName: tableName, Key: keyValue };
+        
+            var data = await docClient.get(params).promise();
+
+            return data.Item;
+        };
+
+        return executeWithRetry(operation, 3, 20, 3);
     } catch (error) {
         throw new Error(error.message);
     }
@@ -120,16 +163,20 @@ exports.QueryTable = async (userCredentials, tableName, keyConditionExpression, 
             sessionToken: userCredentials.sessionToken,
             region: userCredentials.region
         });
-    
-        const params = {
-            TableName: tableName,
-            KeyConditionExpression: keyConditionExpression,
-            ExpressionAttributeValues: expressionAttribute,
-          };
-    
-        var data = await docClient.query(params).promise();
 
-        return data.Items;
+        const operation = async () => {
+            const params = {
+                TableName: tableName,
+                KeyConditionExpression: keyConditionExpression,
+                ExpressionAttributeValues: expressionAttribute,
+            };
+        
+            var data = await docClient.query(params).promise();
+
+            return data.Items;
+        };
+
+        return executeWithRetry(operation, 3, 20, 3);
     } catch (error) {
         throw new Error(error.message);
     }
@@ -143,14 +190,18 @@ exports.BatchQueryTable = async (userCredentials, requestItems) => {
             sessionToken: userCredentials.sessionToken,
             region: userCredentials.region
         });
-    
-        const params = {
-            RequestItems: requestItems
-          };
-    
-        var data = await docClient.batchGet(params).promise();
 
-        return data.Responses;
+        const operation = async () => {
+            const params = {
+                RequestItems: requestItems
+            };
+        
+            var data = await docClient.batchGet(params).promise();
+
+            return data.Responses;
+        };
+
+        return executeWithRetry(operation, 3, 20, 3);
     } catch (error) {
         throw new Error(error.message);
     }
@@ -165,14 +216,18 @@ exports.ScanTable = async (userCredentials, tableName, filterExpression = null) 
             region: userCredentials.region
         });
 
-        const params = { TableName: tableName };
-        if (filterExpression) {
-            params.FilterExpression = filterExpression;
-        }
+        const operation = async () => {
+            const params = { TableName: tableName };
+            if (filterExpression) {
+                params.FilterExpression = filterExpression;
+            }
 
-        const data = await docClient.scan(params).promise();
+            const data = await docClient.scan(params).promise();
 
-        return data.Items;
+            return data.Items;
+        };
+
+        return executeWithRetry(operation, 3, 20, 3);
     } catch (error) {
         throw new Error(error.message);
     }
@@ -186,16 +241,20 @@ exports.UpdateItem = async (userCredentials, tableName, key, updateExpression, e
             sessionToken: userCredentials.sessionToken,
             region: userCredentials.region
         });
-    
-        const params = {
-            TableName: tableName,
-            Key: key,
-            UpdateExpression: updateExpression,
-            ExpressionAttributeValues: expressionAttributeValues,
-            ReturnValues: returnValues
-          };
-    
-        await docClient.update(params).promise();
+
+        const operation = async () => {
+            const params = {
+                TableName: tableName,
+                Key: key,
+                UpdateExpression: updateExpression,
+                ExpressionAttributeValues: expressionAttributeValues,
+                ReturnValues: returnValues
+              };
+        
+            await docClient.update(params).promise();
+        };
+
+        executeWithRetry(operation, 3, 20, 3);
     } catch (error) {
         throw new Error(error.message);
     }
@@ -210,11 +269,15 @@ exports.BatchWriteItems = async (userCredentials, requests) => {
             region: userCredentials.region
         });
 
-        const params = {
-            RequestItems: requests
+        const operation = async () => {
+            const params = {
+                RequestItems: requests
+            };
+
+            await docClient.batchWrite(params).promise();
         };
 
-        await docClient.batchWrite(params).promise();
+        executeWithRetry(operation, 3, 20, 3);
     } catch (error) {
         throw new Error(error.message);
     }
@@ -229,24 +292,28 @@ exports.CreateGSI = async (userCredentials, tableName, indexParams) => {
             region: userCredentials.region
         });
 
-        const params = {
-            TableName: tableName,
-            GlobalSecondaryIndexUpdates: [
-              {
-                Create: {
-                  IndexName: indexParams.IndexName,
-                  KeySchema: indexParams.KeySchema,
-                  Projection: indexParams.Projection,
-                  ProvisionedThroughput: indexParams.ProvisionedThroughput,
+        const operation = async () => {
+            const params = {
+                TableName: tableName,
+                GlobalSecondaryIndexUpdates: [
+                {
+                    Create: {
+                    IndexName: indexParams.IndexName,
+                    KeySchema: indexParams.KeySchema,
+                    Projection: indexParams.Projection,
+                    ProvisionedThroughput: indexParams.ProvisionedThroughput,
+                    },
                 },
-              },
-            ],
-            AttributeDefinitions: indexParams.AttributeDefinitions,
+                ],
+                AttributeDefinitions: indexParams.AttributeDefinitions,
+            };
+
+            const data = await dynamodb.updateTable(params).promise();
+
+            return data;
         };
 
-        const data = await dynamodb.updateTable(params).promise();
-
-        return data;
+        return executeWithRetry(operation, 3, 20, 3);
     } catch (error) {
         throw new Error(error.message);
     }
@@ -261,17 +328,21 @@ exports.UpdateProvisionedThroughPut = async (userCredentials, tableName, readCap
             region: userCredentials.region
         });
 
-        const params = {
-            TableName: tableName,
-            ProvisionedThroughput: {
-              ReadCapacityUnits: readCapacity,
-              WriteCapacityUnits: writeCapacity,
-            },
+        const operation = async () => {
+            const params = {
+                TableName: tableName,
+                ProvisionedThroughput: {
+                ReadCapacityUnits: readCapacity,
+                WriteCapacityUnits: writeCapacity,
+                },
+            };
+
+            const data = await dynamodb.updateTable(params).promise();
+
+            return data;
         };
 
-        const data = await dynamodb.updateTable(params).promise();
-
-        return data;
+        return executeWithRetry(operation, 3, 20, 3);
     } catch (error) {
         throw new Error(error.message);
     }
@@ -286,17 +357,21 @@ exports.EnableStreams = async (userCredentials, tableName, streamViewType) => {
             region: userCredentials.region
         });
 
-        const params = {
-            TableName: tableName,
-            StreamSpecification: {
-              StreamEnabled: true,
-              StreamViewType: streamViewType,
-            },
-          };
+        const operation = async () => {
+            const params = {
+                TableName: tableName,
+                StreamSpecification: {
+                StreamEnabled: true,
+                StreamViewType: streamViewType,
+                },
+            };
 
-        const data = await dynamodb.updateTable(params).promise();
+            const data = await dynamodb.updateTable(params).promise();
 
-        return data;
+            return data;
+        };
+
+        return executeWithRetry(operation, 3, 20, 3);
     } catch (error) {
         throw new Error(error.message);
     }
@@ -311,17 +386,21 @@ exports.UpdateTTL = async (userCredentials, tableName, ttlAttribute) => {
             region: userCredentials.region
         });
 
-        const params = {
-            TableName: tableName,
-            TimeToLiveSpecification: {
-              AttributeName: ttlAttribute,
-              Enabled: true, 
-            },
+        const operation = async () => {
+            const params = {
+                TableName: tableName,
+                TimeToLiveSpecification: {
+                AttributeName: ttlAttribute,
+                Enabled: true, 
+                },
+            };
+
+            const data = await dynamodb.updateTimeToLive(params).promise();
+
+            return data;
         };
 
-        const data = await dynamodb.updateTimeToLive(params).promise();
-
-        return data;
+        return executeWithRetry(operation, 3, 20, 3);
     } catch (error) {
         throw new Error(error.message);
     }
@@ -336,14 +415,18 @@ exports.ModifyTagTable = async (userCredentials, resourceArn, tags) => {
             region: userCredentials.region
         });
 
-        const params = {
-            ResourceArn: resourceArn, 
-            Tags: tags, 
+        const operation = async () => {
+            const params = {
+                ResourceArn: resourceArn, 
+                Tags: tags, 
+            };
+
+            const data = await dynamodb.tagResource(params).promise();
+
+            return data;
         };
 
-        const data = await dynamodb.tagResource(params).promise();
-
-        return data;
+        return executeWithRetry(operation, 3, 20, 3);
     } catch (error) {
         throw new Error(error.message);
     }
@@ -358,11 +441,15 @@ exports.DeleteDynamoDBTable = async (userCredentials, tableName) => {
             region: userCredentials.region
         });
 
-        const params = {
-            TableName: tableName, 
+        const operation = async () => {
+            const params = {
+                TableName: tableName, 
+            };
+
+            await dynamodb.deleteTable(params).promise();
         };
 
-        await dynamodb.deleteTable(params).promise();
+        executeWithRetry(operation, 3, 20, 3);
     } catch (error) {
         throw new Error(error.message);
     }
