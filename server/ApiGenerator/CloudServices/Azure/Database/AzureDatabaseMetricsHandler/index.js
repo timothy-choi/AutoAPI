b8ff540b65c651456b0cb2adb3ff02ccca88aef4
Timothy Choi <tchoi720@gmail.com>
2024-12-14 14:07:44 -0800

    implemented endpoint to get health status of a db instance and starting on impl of sls function to continuously get metrics

---
+++ server/ApiGenerator/CloudServices/Azure/Database/AzureDatabaseMetricsHandler/index.js
@@ -0,0 +1,6 @@
+const { DefaultAzureCredential } = require("@azure/identity");
+const { MonitorManagementClient } = require("@azure/arm-monitor");
+
+module.exports = async function (context, ) {
+    
+}
