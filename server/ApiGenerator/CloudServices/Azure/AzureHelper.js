7bdcc050a389e52d517692f211f0fdf2a66874e9
Timothy Choi <tchoi720@gmail.com>
2024-12-15 23:56:43 -0800

    added a delete and get cosmos db account helper functions

--- server/ApiGenerator/CloudServices/Azure/AzureHelper.js
+++ server/ApiGenerator/CloudServices/Azure/AzureHelper.js
@@ -1,5 +1,6 @@
 const { DefaultAzureCredential } = require("@azure/identity");
 const { OperationalInsightsManagementClient } = require("@azure/arm-operationalinsights");
+const { enableDiagnostics } = require('./Database/AzureRDSMetrics');
 
 exports.createWorkspace = async (resourceGroupName, workspaceName, workspaceParams, subscriptionId) => {
     try {
@@ -13,6 +14,14 @@ exports.createWorkspace = async (resourceGroupName, workspaceName, workspacePara
         );
 
         return workspaceResponse;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
+
+exports.enableLoggingAnalyticsDiagnosis = async (resourceId, subscriptionId, diagnosticRequestInfo, diagnosticRequestName) => {
+    try {
+        await enableDiagnostics(resourceId, subscriptionId, diagnosticRequestInfo, diagnosticRequestName);
     } catch (error) {
         throw new Error(error.message);
     }
--- server/ApiGenerator/CloudServices/Azure/Database/CosmosDB/CosmosDBHelper.js
+++ server/ApiGenerator/CloudServices/Azure/Database/CosmosDB/CosmosDBHelper.js
@@ -17,3 +17,27 @@ exports.CreateOrUpdateCosmosDBAccount = async (resourceGroupName, accountName, c
         throw new Error(error.message);
     }
 };
+
+exports.DeleteCosmosDBAccount = async (resourceGroupName, accountName, subscriptionId) => {
+    try {
+        const credential = new DefaultAzureCredential();
+        const cosmosClient = new CosmosDBManagementClient(credential, subscriptionId);
+
+        await cosmosClient.databaseAccounts.beginDeleteAndWait(resourceGroupName, accountName);
+    } catch (error) {
+        throw new Error(error.message);
+    }
+};
+
+exports.GetCosmosDBAccount = async (resourceGroupName, accountName, subscriptionId) => {
+    try {
+        const credential = new DefaultAzureCredential();
+        const cosmosClient = new CosmosDBManagementClient(credential, subscriptionId);
+
+        var cosmosDbAccount = await cosmosClient.databaseAccounts.get(resourceGroupName, accountName);
+
+        return cosmosDbAccount;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+};
