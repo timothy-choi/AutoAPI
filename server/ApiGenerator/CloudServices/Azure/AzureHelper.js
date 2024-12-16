e219c3cc5f85444aa913f79c7fb028d5aab5764c
Timothy Choi <tchoi720@gmail.com>
2024-12-15 21:46:26 -0800

    added function to create a workspace for all azure services used

---
+++ server/ApiGenerator/CloudServices/Azure/AzureHelper.js
@@ -0,0 +1,19 @@
+const { DefaultAzureCredential } = require("@azure/identity");
+const { OperationalInsightsManagementClient } = require("@azure/arm-operationalinsights");
+
+exports.createWorkspace = async (resourceGroupName, workspaceName, workspaceParams, subscriptionId) => {
+    try {
+        const credential = new DefaultAzureCredential();
+        const operationalInsightsClient = new OperationalInsightsManagementClient(credential, subscriptionId);
+
+        const workspaceResponse = await operationalInsightsClient.workspaces.createOrUpdate(
+            resourceGroupName,
+            workspaceName,
+            workspaceParams
+        );
+
+        return workspaceResponse;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
