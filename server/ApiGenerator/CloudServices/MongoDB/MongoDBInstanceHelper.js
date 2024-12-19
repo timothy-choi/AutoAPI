ee668c0289d8c9bd29d5a739f5a235f07bc00324
Timothy Choi <tchoi720@gmail.com>
2024-12-19 11:43:45 -0800

    added some more mongodb instance helper functions and renamed file

--- server/ApiGenerator/CloudServices/MongoDB/MongoDBInstanceHelper.js
+++ server/ApiGenerator/CloudServices/MongoDB/MongoDBInstanceHelper.js
@@ -41,3 +41,35 @@ exports.createDatabase = async (apiKey, dbName, collectionName, dbUri) => {
         throw new Error(`Failed to create database: ${error.message}`);
       }
 };
+
+exports.deleteDatabase = async (databaseUri) => {
+    try {
+      const apiClient = GetApiClient(apiKey);
+
+      const response = await apiClient.delete(databaseUri, apiKey);
+
+      return response.data;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+};
+
+exports.createCollection = async (apiKey, collectionParams, collectionUri) => {
+    try {
+        const apiClient = GetApiClient(apiKey);
+        
+        const response = await apiClient.post( collectionUri, collectionParams,
+            {
+              headers: {
+                'Authorization': `Bearer ${apiKey}`,
+                'Content-Type': 'application/json',
+              },
+            }
+        );
+
+        return response.data;
+    } catch (error) {
+        throw new Error(`Failed to create database: ${error.message}`);
+    }
+};
+
