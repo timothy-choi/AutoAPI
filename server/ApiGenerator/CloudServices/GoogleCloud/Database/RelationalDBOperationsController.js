e463ec4f5a97c1adfa130cc4fc887e29e562d190
Timothy Choi <tchoi720@gmail.com>
2024-12-22 23:23:53 -0800

    added cloud sql operations endpoints

---
+++ server/ApiGenerator/CloudServices/GoogleCloud/Database/RelationalDBOperationsController.js
@@ -0,0 +1,81 @@
+const axios = require('axios')
+
+exports.ExecuteQuery = async (req, res) => {
+    try {
+        var queryInfo = {
+            connectionInfo: req.body.connectionInfo,
+            query: req.body.query,
+            params: req.body.params
+        };
+
+        var queryResponse = await axios.post("/sqlOperations/query/" + req.database, queryInfo);
+
+        if (queryResponse.status != 201) {
+            return res.status(400).send({"error": queryResponse.json()});
+        }
+
+        return res.status(201).send({"queryResult": queryResponse.json()});
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+};
+
+exports.InsertIntoDB = async (req, res) => {
+    try {
+        var queryInfo = {
+            connectionInfo: req.body.connectionInfo,
+            query: req.body.query,
+            params: req.body.params
+        };
+
+        var queryResponse = await axios.post("/sqlOperations/insert/" + req.database, queryInfo);
+
+        if (queryResponse.status != 201) {
+            return res.status(400).send({"error": queryResponse.json()});
+        }
+
+        return res.status(201).send(null);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+};
+
+exports.ModifyInDB = async (req, res) => {
+    try {
+        var queryInfo = {
+            connectionInfo: req.body.connectionInfo,
+            query: req.body.query,
+            params: req.body.params
+        };
+
+        var modifyResponse = await axios.put("/sqlOperations/modify/" + req.database, queryInfo);
+
+        if (modifyResponse.status != 200) {
+            return res.status(400).send({"error": queryResponse.json()});
+        }
+
+        return res.status(200).send({"modifyResponse": modifyResponse.json()});
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+};
+
+exports.DeleteInDB = async (req, res) => {
+    try {
+        var queryInfo = {
+            connectionInfo: req.body.connectionInfo,
+            query: req.body.query,
+            params: req.body.params
+        };
+
+        var deleteResponse = await axios.put("/sqlOperations/remove/" + req.database, queryInfo);
+
+        if (deleteResponse.status != 200) {
+            return res.status(400).send({"error": deleteResponse.json()});
+        }
+
+        return res.status(200).send({"deleteResponse": deleteResponse.json()});
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+};
