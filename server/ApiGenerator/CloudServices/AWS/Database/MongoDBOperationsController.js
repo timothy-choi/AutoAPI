51cd7966efa10a30d17e6248bd5c0b49b3311332
Timothy Choi <tchoi720@gmail.com>
2024-12-25 18:31:34 -0800

    added endpoitns for atomic operations

--- server/ApiGenerator/CloudServices/AWS/Database/MongoDBOperationsController.js
+++ server/ApiGenerator/CloudServices/AWS/Database/MongoDBOperationsController.js
@@ -14,7 +14,7 @@ exports.QueryOne = async (req, res) => {
             region: req.body.userRegion
         });
 
-        if (!("filter" in req.body.payloadInfo) || (req.body.payloadInfo.filter == null || req.body.payloadInfo.projection == null)) {
+        if (!("filter" in req.body.payloadInfo) || req.body.payloadInfo.filter == null || req.body.payloadInfo.projection == null) {
             return res.status(400).send({"error": "invalid input"});
         }
 
@@ -51,7 +51,7 @@ exports.QueryMany = async (req, res) => {
             region: req.body.userRegion
         });
 
-        if (!("filter" in req.body.payloadInfo) || (req.body.payloadInfo.filter == null || req.body.payloadInfo.options == null)) {
+        if (!("filter" in req.body.payloadInfo) || req.body.payloadInfo.filter == null || req.body.payloadInfo.options == null) {
             return res.status(400).send({"error": "invalid input"});
         }
 
@@ -88,7 +88,7 @@ exports.Aggregate = async (req, res) => {
             region: req.body.userRegion
         });
 
-        if (!("pipeline" in req.body.payloadInfo) || (req.body.payloadInfo.pipeline == null || req.body.payloadInfo.pipeline == {})) {
+        if (!("pipeline" in req.body.payloadInfo) || req.body.payloadInfo.pipeline == null || req.body.payloadInfo.pipeline == {}) {
             return res.status(400).send({"error": "invalid input"});
         }
 
@@ -125,7 +125,7 @@ exports.CountDocuments = async (req, res) => {
             region: req.body.userRegion
         });
 
-        if (!("query" in req.body.payloadInfo) || (req.body.payloadInfo.query == null || req.body.payloadInfo.query == "")) {
+        if (!("query" in req.body.payloadInfo) || req.body.payloadInfo.query == null || req.body.payloadInfo.query == "") {
             return res.status(400).send({"error": "invalid input"});
         }
 
@@ -199,7 +199,7 @@ exports.InsertMany = async (req, res) => {
             region: req.body.userRegion
         });
 
-        if (!("documents" in req.body.payloadInfo) || (req.body.payloadInfo.documents == null || req.body.payloadInfo.documents.length == 0)) {
+        if (!("documents" in req.body.payloadInfo) || req.body.payloadInfo.documents == null || req.body.payloadInfo.documents.length == 0) {
             return res.status(400).send({"error": "invalid input"});
         }
 
@@ -438,6 +438,191 @@ exports.BulkWrite = async (req, res) => {
 
 
         return res.status(201).send({"queryResponse": queryResponse});
+    } catch (error) {
+        return res.status(500).send(null);
+    }
+};
+
+exports.IncrementField = async (req, res) => {
+    try {
+        let userCredentials = {};
+        if (req.body.userCredentialsInfo) {
+            userCredentials = req.body.userCredentialsInfo;
+        } else {
+            userCredentials = await AWSHelper.getAWSCredentials(req.body.secretAccessName);
+        }
+
+        const lambda = new AWS.Lambda({
+            credentials: new AWS.Credentials(userCredentials.accessKey, userCredentials.userSecretKey, userCredentials.sessionToken),
+            region: req.body.userRegion
+        });
+
+        if (!("query" in req.body.payloadInfo) || (req.body.payloadInfo.query == null || !("field" in req.body.payloadInfo) || req.body.payloadInfo.field == null || !("incrementBy" in req.body.payloadInfo) || req.body.payloadInfo.incrementBy == null)) {
+            return res.status(400).send({"error": "invalid input"});
+        }
+
+        const params = {
+            FunctionName: req.body.lambdaFunctionName,
+            Payload: JSON.stringify(req.body.payloadInfo)
+        };
+
+        const lambdaResponse = await lambda.invoke(params).promise();
+        const queryResponse = JSON.parse(lambdaResponse.Payload);
+
+        if (queryResponse.status !== 200) {
+            return res.status(500).send({ error: "Could not get usage report and health status" });
+        }
+
+
+        return res.status(200).send({"queryResponse": queryResponse});
+    } catch (error) {
+        return res.status(500).send(null);
+    }
+};
+
+exports.SetField = async (req, res) => {
+    try {
+        let userCredentials = {};
+        if (req.body.userCredentialsInfo) {
+            userCredentials = req.body.userCredentialsInfo;
+        } else {
+            userCredentials = await AWSHelper.getAWSCredentials(req.body.secretAccessName);
+        }
+
+        const lambda = new AWS.Lambda({
+            credentials: new AWS.Credentials(userCredentials.accessKey, userCredentials.userSecretKey, userCredentials.sessionToken),
+            region: req.body.userRegion
+        });
+
+        if (!("query" in req.body.payloadInfo) || (req.body.payloadInfo.query == null || !("field" in req.body.payloadInfo) || req.body.payloadInfo.field == null || !("value" in req.body.payloadInfo) || req.body.payloadInfo.value == null)) {
+            return res.status(400).send({"error": "invalid input"});
+        }
+
+        const params = {
+            FunctionName: req.body.lambdaFunctionName,
+            Payload: JSON.stringify(req.body.payloadInfo)
+        };
+
+        const lambdaResponse = await lambda.invoke(params).promise();
+        const queryResponse = JSON.parse(lambdaResponse.Payload);
+
+        if (queryResponse.status !== 200) {
+            return res.status(500).send({ error: "Could not get usage report and health status" });
+        }
+
+
+        return res.status(200).send({"queryResponse": queryResponse});
+    } catch (error) {
+        return res.status(500).send(null);
+    }
+};
+
+exports.AddToArray = async (req, res) => {
+    try {
+        let userCredentials = {};
+        if (req.body.userCredentialsInfo) {
+            userCredentials = req.body.userCredentialsInfo;
+        } else {
+            userCredentials = await AWSHelper.getAWSCredentials(req.body.secretAccessName);
+        }
+
+        const lambda = new AWS.Lambda({
+            credentials: new AWS.Credentials(userCredentials.accessKey, userCredentials.userSecretKey, userCredentials.sessionToken),
+            region: req.body.userRegion
+        });
+
+        if (!("query" in req.body.payloadInfo) || (req.body.payloadInfo.query == null || !("field" in req.body.payloadInfo) || req.body.payloadInfo.field == null || !("value" in req.body.payloadInfo) || req.body.payloadInfo.value == null)) {
+            return res.status(400).send({"error": "invalid input"});
+        }
+
+        const params = {
+            FunctionName: req.body.lambdaFunctionName,
+            Payload: JSON.stringify(req.body.payloadInfo)
+        };
+
+        const lambdaResponse = await lambda.invoke(params).promise();
+        const queryResponse = JSON.parse(lambdaResponse.Payload);
+
+        if (queryResponse.status !== 200) {
+            return res.status(500).send({ error: "Could not get usage report and health status" });
+        }
+
+
+        return res.status(200).send({"queryResponse": queryResponse});
+    } catch (error) {
+        return res.status(500).send(null);
+    }
+};
+
+exports.RemoveFromArray = async (req, res) => {
+    try {
+        let userCredentials = {};
+        if (req.body.userCredentialsInfo) {
+            userCredentials = req.body.userCredentialsInfo;
+        } else {
+            userCredentials = await AWSHelper.getAWSCredentials(req.body.secretAccessName);
+        }
+
+        const lambda = new AWS.Lambda({
+            credentials: new AWS.Credentials(userCredentials.accessKey, userCredentials.userSecretKey, userCredentials.sessionToken),
+            region: req.body.userRegion
+        });
+
+        if (!("query" in req.body.payloadInfo) || (req.body.payloadInfo.query == null || !("field" in req.body.payloadInfo) || req.body.payloadInfo.field == null || !("value" in req.body.payloadInfo) || req.body.payloadInfo.value == null)) {
+            return res.status(400).send({"error": "invalid input"});
+        }
+
+        const params = {
+            FunctionName: req.body.lambdaFunctionName,
+            Payload: JSON.stringify(req.body.payloadInfo)
+        };
+
+        const lambdaResponse = await lambda.invoke(params).promise();
+        const queryResponse = JSON.parse(lambdaResponse.Payload);
+
+        if (queryResponse.status !== 200) {
+            return res.status(500).send({ error: "Could not get usage report and health status" });
+        }
+
+
+        return res.status(200).send({"queryResponse": queryResponse});
+    } catch (error) {
+        return res.status(500).send(null);
+    }
+};
+
+exports.UpdateArrayElement = async (req, res) => {
+    try {
+        let userCredentials = {};
+        if (req.body.userCredentialsInfo) {
+            userCredentials = req.body.userCredentialsInfo;
+        } else {
+            userCredentials = await AWSHelper.getAWSCredentials(req.body.secretAccessName);
+        }
+
+        const lambda = new AWS.Lambda({
+            credentials: new AWS.Credentials(userCredentials.accessKey, userCredentials.userSecretKey, userCredentials.sessionToken),
+            region: req.body.userRegion
+        });
+
+        if (!("query" in req.body.payloadInfo) || req.body.payloadInfo.query == null || !("field" in req.body.payloadInfo) || req.body.payloadInfo.field == null || !("oldValue" in req.body.payloadInfo) || req.body.payloadInfo.oldValue == null || !("newValue" in req.body.payloadInfo) || req.body.payloadInfo.newValue == null) {
+            return res.status(400).send({"error": "invalid input"});
+        }
+
+        const params = {
+            FunctionName: req.body.lambdaFunctionName,
+            Payload: JSON.stringify(req.body.payloadInfo)
+        };
+
+        const lambdaResponse = await lambda.invoke(params).promise();
+        const queryResponse = JSON.parse(lambdaResponse.Payload);
+
+        if (queryResponse.status !== 200) {
+            return res.status(500).send({ error: "Could not get usage report and health status" });
+        }
+
+
+        return res.status(200).send({"queryResponse": queryResponse});
     } catch (error) {
         return res.status(500).send(null);
     }
