a8209f35ba7269d0494386eb799b902af7d42148
Timothy Choi <tchoi720@gmail.com>
2024-12-29 15:46:36 -0800

    added group, user, and users endpoints

--- server/ApiGenerator/ProjectManagementController.js
+++ server/ApiGenerator/ProjectManagementController.js
@@ -30,6 +30,66 @@ exports.CreateProjectManagement = async (req, res) => {
     }
 };
 
+exports.setGroupId = async (req, res) => {
+    try {
+        await ProjectManagementHelper.setGroupId(req.projectManagementId, req.groupId);
+
+        return res.status(200).send(null);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+};
+
+exports.setUserId = async (req, res) => {
+    try {
+        await ProjectManagementHelper.setUserId(req.projectManagementId, req.userId);
+
+        return res.status(200).send(null);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+exports.updateProjectStatsInfo = async (req, res) => {
+    try {
+        await ProjectManagementHelper.updateProjectStatsInfo(req.projectManagementId, req.body.projectStatsInfo);
+
+        return res.status(200).send(null);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+exports.addUserToProject = async (req, res) => {
+    try {
+        await ProjectManagementHelper.AddUser(req.projectManagementId, req.body);
+
+        return res.status(200).send(null);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+};
+
+exports.updateUserInProject = async (req, res) => {
+    try {
+        await ProjectManagementHelper.UpateUser(req.projectManagementId, req.body);
+
+        return res.status(200).send(null);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+};
+
+exports.deleteUserFromProject = async (req, res) => {
+    try {
+        await ProjectManagementHelper.DeleteUser(req.projectManagementId, req.userId);
+
+        return res.status(200).send(null);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+};
+
 exports.deleteProjectManagement = async (req, res) => {
     try {
         await ProjectManagementHelper.deleteProjectManagement(req.projectManagementId);
