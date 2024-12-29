f264fcd2d9a4ca820f6f9b65237992637d4e8c53
Timothy Choi <tchoi720@gmail.com>
2024-12-29 14:37:46 -0800

    added helper functions for adding log entries

--- server/ApiGenerator/ProjectManagementService.js
+++ server/ApiGenerator/ProjectManagementService.js
@@ -63,6 +63,126 @@ exports.setUserId = async (projectManagementId, userId) => {
     }
 }
 
+exports.updateProjectStatsInfo = async (projectManagementId, projectStatsInfo) => {
+    try {
+        var projectManagement = await this.GetProjectManagementById(projectManagementId);
+
+        if (!projectManagement) {
+            throw new Error('Instance does not exist');
+        }
+
+        projectManagement.ProjectStatsInfo = projectStatsInfo;
+
+        projectManagement.UpdatedAt = Date.now();
+
+        await projectManagement.save();
+    } catch (error) {
+        throw new Error('Error updating project stats info:', error);
+    }
+}
+
+exports.AddUser = async (projectManagementId, user) => {
+    try {
+        var projectManagement = await this.GetProjectManagementById(projectManagementId);
+
+        if (!projectManagement) {
+            throw new Error('Instance does not exist');
+        }
+
+        projectManagement.AllUsers.push(user);
+
+        await projectManagement.save();
+    } catch (error) {
+        throw new Error('Error adding user:', error);
+    }
+}
+
+exports.UpateUser = async (projectManagementId, user) => {
+    try {
+        var projectManagement = await this.GetProjectManagementById(projectManagementId);
+
+        if (!projectManagement) {
+            throw new Error('Instance does not exist');
+        }
+
+        var userIndex = projectManagement.AllUsers.findIndex(u => u.id === user.id);
+
+        if (userIndex < 0) {
+            throw new Error('User does not exist');
+        }
+
+        projectManagement.AllUsers[userIndex] = user;
+
+        await projectManagement.save();
+    } catch (error) {
+        throw new Error('Error updating user:', error);
+    }
+}
+
+exports.DeleteUser = async (projectManagementId, userId) => {
+    try {
+        var projectManagement = await this.GetProjectManagementById(projectManagementId);
+
+        if (!projectManagement) {
+            throw new Error('Instance does not exist');
+        }
+
+        projectManagement.AllUsers = projectManagement.AllUsers.filter(u => u.id != userId);
+
+        await projectManagement.save();
+    } catch (error) {
+        throw new Error('Error deleting user:', error);
+    }
+}
+
+exports.AddToApiMonitoringLog = async (projectManagementId, logEntry) => {
+    try {
+        var projectManagement = await this.GetProjectManagementById(projectManagementId);
+
+        if (!projectManagement) {
+            throw new Error('Instance does not exist');
+        }
+
+        projectManagement.ApiMonitoringLog.push(logEntry);
+
+        await projectManagement.save();
+    } catch (error) {
+        throw new Error('Error adding to api monitoring log:', error);
+    }
+}
+
+exports.AddToErrorLog = async (projectManagementId, logEntry) => {
+    try {
+        var projectManagement = await this.GetProjectManagementById(projectManagementId);
+
+        if (!projectManagement) {
+            throw new Error('Instance does not exist');
+        }
+
+        projectManagement.ErrorLog.push(logEntry);
+
+        await projectManagement.save();
+    } catch (error) {
+        throw new Error('Error adding to error log:', error);
+    }
+}
+
+exports.AddToSecurityIncidentLog = async (projectManagementId, logEntry) => {
+    try {
+        var projectManagement = await this.GetProjectManagementById(projectManagementId);
+
+        if (!projectManagement) {
+            throw new Error('Instance does not exist');
+        }
+
+        projectManagement.SecurityIncidentLog.push(logEntry);
+
+        await projectManagement.save();
+    } catch (error) {
+        throw new Error('Error adding to security incident log:', error);
+    }
+}
+
 exports.deleteProjectManagement = async (projectManagementId) => {
     try {
         var projectManagement = await this.GetProjectManagementById(projectManagementId);
