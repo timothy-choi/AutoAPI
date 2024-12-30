a71c7038f812d2e53ba297d2ce11d8d9e6dbe735
Timothy Choi <tchoi720@gmail.com>
2024-12-29 18:03:33 -0800

    added some more project stats endpoints

--- server/ApiGenerator/ProjectStatsController.js
+++ server/ApiGenerator/ProjectStatsController.js
@@ -30,6 +30,76 @@ exports.CreateProjectStats = async (req, res) => {
     }
 };
 
+exports.setNumberOfViews = async (req, res) => {
+    try {
+        await projectStatsService.SetNumberOfViews(req.projectStatsId);
+
+        return res.status(200).send(null);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+};
+
+exports.setNumberOfApiCalls = async (req, res) => {
+    try {
+        await projectStatsService.SetNumberOfApiCalls(req.projectStatsId);
+
+        return res.status(200).send(null);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+};
+
+exports.addApiEndpointUsage = async (req, res) => {
+    try {
+        await projectStatsService.AddApiEndpointUsage(req.projectStatsId, req.body);
+
+        return res.status(200).send(null);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+};
+
+exports.removeApiEndpointUsage = async (req, res) => {
+    try {
+        await projectStatsService.RemoveApiEndpointUsage(req.projectStatsId, req.apiEndpointUsageId);
+
+        return res.status(200).send(null);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+};
+
+exports.updateApiEndpointUsage = async (req, res) => {
+    try {
+        await projectStatsService.EditApiEndpointUsage(req.projectStatsId, req.apiEndpointUsageId, req.body);
+
+        return res.status(200).send(null);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+};
+
+exports.addContributorStats = async (req, res) => {
+    try {
+        await projectStatsService.AddContributorStats(req.projectStatsId, req.body);
+
+        return res.status(200).send(null);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+};
+
+exports.editContributorStats = async (req, res) => {
+    try {
+        await projectStatsService.EditContributorStats(req.projectStatsId, req.contributorStatsId, req.body);
+
+        return res.status(200).send(null);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
 exports.deleteProjectStats = async (req, res) => {
     try {
         await projectStatsService.DeleteProjectStats(req.projectStatsId);
