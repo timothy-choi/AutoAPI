93ddeae1f7d928b80b3ffefc2c863966fbd3cb02
Timothy Choi <tchoi720@gmail.com>
2025-01-02 13:00:43 -0800

    added get gcloud function operations

--- server/ApiGenerator/CloudServices/GoogleCloud/ServerlessFunction/ServerlessFunctionHelper.js
+++ server/ApiGenerator/CloudServices/GoogleCloud/ServerlessFunction/ServerlessFunctionHelper.js
@@ -2,7 +2,7 @@ const { CloudFunctionsServiceClient } = require('@google-cloud/functions');
 const { Storage } = require('@google-cloud/storage');
 const { exec } = require('child_process');
 
-exports.DeployServerlessFunction = async (functionName, entryPoint, runtime, sourcePath) => {
+exports.DeployServerlessFunction = async (functionName, entryPoint, runtime, sourcePath, region, projectId) => {
     try {
         const deployCommand = `
             gcloud functions deploy ${functionName} \
@@ -30,9 +30,37 @@ exports.DeployServerlessFunction = async (functionName, entryPoint, runtime, sou
 exports.DeleteServerlessFunction = async (functionName, projectId, region) => {
     try {
         const client = new CloudFunctionsServiceClient();
-        
+
         const [response] = await client.deleteFunction({
             name: `projects/${projectId}/locations/${region}/functions/${functionName}`,
+        });
+
+        return response;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+};
+
+exports.GetServerlessFunction = async (functionName, projectId, region) => {
+    try {
+        const client = new CloudFunctionsServiceClient();
+
+        const [response] = await client.getFunction({
+            name: `projects/${projectId}/locations/${region}/functions/${functionName}`,
+        });
+
+        return response;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+};
+
+exports.listServerlessFunctions = async (projectId, region) => {
+    try {
+        const client = new CloudFunctionsServiceClient();
+
+        const [response] = await client.listFunctions({
+            parent: `projects/${projectId}/locations/${region}`,
         });
 
         return response;
