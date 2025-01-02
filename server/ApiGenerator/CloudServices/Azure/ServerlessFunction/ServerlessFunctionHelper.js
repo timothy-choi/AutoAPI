bb8de98e6bfd4e053ce5c0bcf7c18ce1992634f3
Timothy Choi <tchoi720@gmail.com>
2025-01-01 23:39:25 -0800

    added a function impl to azure to create a function app.

--- server/ApiGenerator/CloudServices/Azure/ServerlessFunction/ServerlessFunctionHelper.js
+++ server/ApiGenerator/CloudServices/Azure/ServerlessFunction/ServerlessFunctionHelper.js
@@ -14,3 +14,32 @@ exports.createStorageAccount = async (storageAccountName, storageConfig, resourc
         throw new Error(error.message);
     }
 };
+
+exports.createFunctionApp = async (functionAppName, storageAccountName, resourceGroupName, appServicePlanConfig, location, subscriptionId) => {
+    try {
+        const appServicePlanName = `${functionAppName}-plan`;
+
+        const credential = new DefaultAzureCredential();
+
+        const client = new WebSiteManagementClient(credential, subscriptionId);
+
+        await client.appServicePlans.beginCreateOrUpdateAndWait(resourceGroupName, appServicePlanName, appServicePlanConfig);
+
+        var response = await client.webApps.beginCreateOrUpdateAndWait(resourceGroupName, functionAppName, {
+            location: location,
+            serverFarmId: `/subscriptions/${subscriptionId}/resourceGroups/${resourceGroupName}/providers/Microsoft.Web/serverfarms/${appServicePlanName}`,
+            siteConfig: {
+                appSettings: [
+                    { name: 'FUNCTIONS_EXTENSION_VERSION', value: '~4' },
+                    { name: 'WEBSITE_RUN_FROM_PACKAGE', value: '1' },
+                    { name: 'AzureWebJobsStorage', value: `DefaultEndpointsProtocol=https;AccountName=${storageAccountName}` },
+                ],
+            },
+            httpsOnly: true,
+        });
+
+        return response;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+};
