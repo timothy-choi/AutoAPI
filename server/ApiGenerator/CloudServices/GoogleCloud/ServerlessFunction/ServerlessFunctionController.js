c2b4a4be34bae35329c4f8a0b119b2769678e2c6
Timothy Choi <tchoi720@gmail.com>
2025-01-03 15:57:39 -0800

    adding create, update, and delete gcloud function endpoints

--- server/ApiGenerator/CloudServices/GoogleCloud/ServerlessFunction/ServerlessFunctionController.js
+++ server/ApiGenerator/CloudServices/GoogleCloud/ServerlessFunction/ServerlessFunctionController.js
@@ -19,3 +19,33 @@ exports.ListServerlessFunctions = async (req, res) => {
         return res.status(500).send(error.message);
     }
 };
+
+exports.DeployServerlessFunction = async (req, res) => {
+    try {
+        await serverlessFunctionHelper.deployFunction(req.body.functionName, req.body.entryPoint, req.body.runtime, req.body.sourcePath, req.body.region, req.body.projectId);
+
+        return res.status(201).send(null);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+};
+
+exports.UpdateServerlessFunction = async (req, res) => {
+    try {
+        var response = await serverlessFunctionHelper.UpdateServerlessFunction(req.body.functionName, req.body.projectId, req.body.region, req.body.updatedConfig);
+
+        return res.status(200).send(response);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+};
+
+exports.DeleteServerlessFunction = async (req, res) => {
+    try {
+        await serverlessFunctionHelper.deleteFunction(req.functionName, req.projectId, req.region);
+
+        return res.status(200).send(null);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+};
