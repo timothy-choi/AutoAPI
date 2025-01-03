cf52162237dc0f0fd437315731c280ad8043131f
Timothy Choi <tchoi720@gmail.com>
2025-01-03 11:28:37 -0800

    added aws s3 helper functions to aws helper file

--- server/ApiGenerator/CloudServices/AWS/AWSHelperFunctions.js
+++ server/ApiGenerator/CloudServices/AWS/AWSHelperFunctions.js
@@ -1,5 +1,6 @@
 const AWS = require('aws-sdk');
-
+const fs = require('fs');
+const path = require('path');
 const axios = require('axios');
 
 exports.createVPC = async (cidrBlock, vpcName, userCredentials, userRegion) => {
@@ -356,3 +357,186 @@ exports.createSubscriptionFilter = async (logGroupName, filterName, filterPatter
         throw new Error(error.message);
     }
 };
+
+exports.createBucket = async (bucketName, userCredentials, userRegion) => {
+    const s3 = new AWS.S3({
+        credentials: new AWS.Credentials(userCredentials.accessKey, userCredentials.userSecretKey, userCredentials.sessionToken),
+        region: userRegion
+    });
+
+    const params = {
+      Bucket: bucketName,
+      CreateBucketConfiguration: {
+        LocationConstraint: 'us-west-1'
+      }
+    };
+  
+    try {
+      await s3.createBucket(params).promise();
+    } catch (error) {
+      console.error('Error creating bucket:', error);
+    }
+};
+
+exports.deleteBucket = async (bucketName, userCredentials, userRegion) => {
+    try {
+      const s3 = new AWS.S3({
+          credentials: new AWS.Credentials(userCredentials.accessKey, userCredentials.userSecretKey, userCredentials.sessionToken),
+          region: userRegion
+      });
+
+      const listParams = {
+        Bucket: bucketName
+      };
+  
+      const listedObjects = await s3.listObjectsV2(listParams).promise();
+  
+      if (listedObjects.Contents.length > 0) {
+        const deleteParams = {
+          Bucket: bucketName,
+          Delete: {
+            Objects: listedObjects.Contents.map(item => ({ Key: item.Key }))
+          }
+        };
+        await s3.deleteObjects(deleteParams).promise();
+      }
+  
+      const deleteBucketParams = {
+        Bucket: bucketName
+      };
+      await s3.deleteBucket(deleteBucketParams).promise();
+    } catch (error) {
+      throw new Error('Error deleting bucket');
+    }
+}; 
+
+exports.bucketExists = async (bucketName, userCredentials, userRegion) => {
+    try {
+        const s3 = new AWS.S3({
+            credentials: new AWS.Credentials(userCredentials.accessKey, userCredentials.userSecretKey, userCredentials.sessionToken),
+            region: userRegion
+        });
+
+      await s3.headBucket({ Bucket: bucketName }).promise();
+
+      return true;
+    } catch (error) {
+      if (error.code === 'NotFound') {
+        return false
+      } else {
+        throw new Error('Error checking bucket existence:', error);
+      }
+    }
+  };
+
+exports.uploadFile = async (bucketName, key, fileContent, userCredentials, userRegion) => {
+    const s3 = new AWS.S3({
+        credentials: new AWS.Credentials(userCredentials.accessKey, userCredentials.userSecretKey, userCredentials.sessionToken),
+        region: userRegion
+    });
+
+    const params = {
+        Bucket: bucketName,
+        Key: key,
+        Body: fileContent,
+        ContentType: 'application/octet-stream'
+    };
+
+    try {
+        await s3.upload(params).promise();
+    } catch (error) {
+        throw new Error('Error uploading file:', error);
+    }
+};
+
+exports.downloadFile = async (bucketName, key, userCredentials, userRegion) => {
+    const s3 = new AWS.S3({
+        credentials: new AWS.Credentials(userCredentials.accessKey, userCredentials.userSecretKey, userCredentials.sessionToken),
+        region: userRegion
+    });
+
+    const params = {
+        Bucket: bucketName,
+        Key: key
+    };
+  
+    try {
+        const data = await s3.getObject(params).promise();
+        return data.Body; 
+    } catch (error) {
+        throw new Error('Error downloading file:', error);
+    }
+};
+
+exports.deleteFile = async (bucketName, key, userCredentials, userRegion) => {
+    const s3 = new AWS.S3({
+        credentials: new AWS.Credentials(userCredentials.accessKey, userCredentials.userSecretKey, userCredentials.sessionToken),
+        region: user
+    });
+
+    const params = {
+        Bucket: bucketName,
+        Key: key
+    };
+  
+    try {
+        await s3.deleteObject(params).promise();
+    } catch (error) {
+        throw new Error('Error deleting file:', error);
+    }
+};
+
+exports.checkFileExists = async (bucketName, key, userCredentials, userRegion) => {
+    try {
+        const s3 = new AWS.S3({
+            credentials: new AWS.Credentials(userCredentials.accessKey, userCredentials.userSecretKey, userCredentials.sessionToken),
+            region: userRegion
+        });
+
+        await s3.headObject({ Bucket: bucketName, Key: key }).promise();
+
+        return true;
+    } catch (error) {
+        if (error.code === 'NotFound') {
+            return false;
+        }
+        throw new Error('Error checking if file exists:', error);
+    }
+};
+
+exports.updateFile = async (bucketName, key, updatedContent, userCredentials, userRegion) => {
+    const s3 = new AWS.S3({
+        credentials: new AWS.Credentials(userCredentials.accessKey, userCredentials.userSecretKey, userCredentials.sessionToken),
+        region: userRegion
+    });
+
+    const params = {
+        Bucket: bucketName,
+        Key: key,
+        Body: updatedContent,
+        ContentType: 'application/octet-stream'
+    };
+    
+    try {
+        await s3.putObject(params).promise();
+    } catch (error) {
+        throw new Error('Error updating file:', error);
+    }
+};
+
+exports.checkFileExists = async (bucketName, key, userCredentials, userRegion) => {
+    try {
+        const s3 = new AWS.S3({
+            credentials: new AWS.Credentials(userCredentials.accessKey, userCredentials.userSecretKey, userCredentials.sessionToken),
+            region: userRegion
+        });
+        
+      await s3.headObject({ Bucket: bucketName, Key: key }).promise();
+      return true; 
+    } catch (error) {
+      if (error.code === 'NotFound') {
+        return false; 
+      }
+      throw new Error('Error checking file existence:', error);
+    }
+  };
