9c37753fc8b9b8fed19784e59253b6ede74f34da
Timothy Choi <tchoi720@gmail.com>
2025-01-08 21:59:27 -0800

    added github endpoint to create a repo for a project

--- server/Github/GithubController.js
+++ server/Github/GithubController.js
@@ -69,3 +69,33 @@ exports.getGithubRepo = async (req, res) => {
         return res.status(500).send(error.message);
     }
 }
+
+exports.createGithubRepo = async (req, res) => {
+    try {
+        const authHeader = req.headers['authorization'];
+
+        if (!authHeader) {
+            return res.status(401).send('Authorization header missing');
+        }
+        
+        let token;
+        
+        if (authHeader.startsWith('Bearer ')) {
+            token = authHeader.split(' ')[1];
+        } else if (authHeader.startsWith('token ')) {
+            token = authHeader.split(' ')[1];
+        } else {
+            return res.status(400).send('Invalid Authorization header format');
+        }
+    
+        if (!token) {
+            return res.status(401).send('Token not found');
+        }
+
+        const githubRepo = await GithubHelper.createGithubRepo(token, req.body.repoInfo);
+
+        return res.status(201).send(githubRepo);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+};
--- server/Github/GithubHelper.js
+++ server/Github/GithubHelper.js
@@ -35,3 +35,15 @@ exports.getGithubRepo = async (accessToken, owner, repo) => {
         throw new Error("Error:", error.message);
     }
 };
+
+exports.createGithubRepo = async (accessToken, repoInfo) => {
+    const octokit = new OctoKit({ auth: accessToken });
+
+    try {
+        const response = await octokit.rest.repos.createForAuthenticatedUser(repoInfo);
+
+        return response.data;
+    } catch (error) {
+        throw new Error("Error:", error.message);
+    }
+};
