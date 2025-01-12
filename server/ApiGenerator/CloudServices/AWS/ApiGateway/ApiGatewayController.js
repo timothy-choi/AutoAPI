f89b3583ec4a42c8395b16568250608b18e9a94a
Timothy Choi <tchoi720@gmail.com>
2025-01-12 13:29:01 -0800

    added create and delete aws api gateway endpoints

--- server/ApiGenerator/CloudServices/AWS/ApiGateway/ApiGatewayController.js
+++ server/ApiGenerator/CloudServices/AWS/ApiGateway/ApiGatewayController.js
@@ -9,3 +9,23 @@ exports.getApiGateway = async (req, res) => {
         return res.status(500).send(error.message);
     }
 };
+
+exports.createApiGateway = async (req, res) => {
+    try {
+        const apiGateway = await ApiGatewayHelper.createApiGateway(req.body.apiName, req.body.description, req.body.endpointConfig, req.body.userCredentials, req.body.userRegion);
+
+        return res.status(201).send(apiGateway);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+};
+
+exports.deleteApiGateway = async (req, res) => {
+    try {
+        await ApiGatewayHelper.deleteApiGateway(req.body.apiGatewayId, req.body.userCredentials, req.body.userRegion);
+
+        return res.status(200).send(null);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+};
--- server/ApiGenerator/CloudServices/AWS/ApiGateway/ApiGatewayHelper.js
+++ server/ApiGenerator/CloudServices/AWS/ApiGateway/ApiGatewayHelper.js
@@ -15,3 +15,37 @@ exports.getApiGateway = async (gatewayId, userCredentials, userRegion) => {
         throw new Error("Error:", error.message);
     }
 };
+
+exports.createApiGateway = async (apiName, description, endpointConfig, userCredentials, userRegion) => {
+    try {
+        const apiGateway = new AWS.APIGateway({ credentials: userCredentials, region: userRegion });
+
+        const params = {
+            name: apiName,
+            description: description,
+            endpointConfiguration: endpointConfig
+        };
+
+        const response = await apiGateway.createRestApi(params).promise();
+
+        return response;
+    } catch (error) {
+        throw new Error("Error:", error.message);
+    }
+};
+
+exports.deleteApiGateway = async (gatewayId, userCredentials, userRegion) => {
+    try {
+        const apiGateway = new AWS.APIGateway({ credentials: userCredentials, region: userRegion });
+
+        const params = {
+            restApiId: gatewayId
+        };
+
+        const response = await apiGateway.deleteRestApi(params).promise();
+
+        return response;
+    } catch (error) {
+        throw new Error("Error:", error.message);
+    }
+};
