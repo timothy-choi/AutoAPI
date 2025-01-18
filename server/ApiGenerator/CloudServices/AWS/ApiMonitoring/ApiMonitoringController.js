f6013a772d8e0ab9b6992d555a487355f17e03cb
Timothy Choi <tchoi720@gmail.com>
2025-01-18 02:56:06 -0800

    also started impl aws api monitoring controller

---
+++ server/ApiGenerator/CloudServices/AWS/ApiMonitoring/ApiMonitoringController.js
@@ -0,0 +1,52 @@
+const ApiMonitoringHelper = require('./ApiMonitoringHelper');
+
+exports.CreateTrail = async (req, res) => {
+    try {
+        let userCredentials = {};
+        if (req.body.userCredentialsInfo) {
+            userCredentials = req.body.userCredentialsInfo;
+        } else {
+            userCredentials = await AWSHelper.getAWSCredentials(req.body.secretName);
+        }
+
+        var trail = await ApiMonitoringHelper.createTrail(userCredentials, req.body.trailInfo, req.body.region);
+
+        return res.status(201).send(trail);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+};
+
+exports.StartLogging = async (req, res) => {
+    try {
+        let userCredentials = {};
+        if (req.body.userCredentialsInfo) {
+            userCredentials = req.body.userCredentialsInfo;
+        } else {
+            userCredentials = await AWSHelper.getAWSCredentials(req.body.secretName);
+        }
+
+        await ApiMonitoringHelper.startLogging(userCredentials, req.body.trailName, req.body.region);
+
+        return res.status(201).send(null);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+};
+
+exports.StopLogging = async (req, res) => {
+    try {
+        let userCredentials = {};
+        if (req.body.userCredentialsInfo) {
+            userCredentials = req.body.userCredentialsInfo;
+        } else {
+            userCredentials = await AWSHelper.getAWSCredentials(req.body.secretName);
+        }
+
+        await ApiMonitoringHelper.stopLogging(userCredentials, req.body.trailName, req.body.region);
+
+        return res.status(200).send(null);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+};
