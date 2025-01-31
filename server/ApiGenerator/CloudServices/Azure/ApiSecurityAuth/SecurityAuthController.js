10cf158331962a5f03339b5e5b7d979741234d7d
Timothy Choi <tchoi720@gmail.com>
2025-01-30 16:44:34 -0800

    added jwt endpoints for azure security auth service

--- server/ApiGenerator/CloudServices/Azure/ApiSecurityAuth/SecurityAuthController.js
+++ server/ApiGenerator/CloudServices/Azure/ApiSecurityAuth/SecurityAuthController.js
@@ -19,3 +19,23 @@ exports.DeleteApiKey = async (req, res) => {
         return res.status(500).send(error.message);
     }
 };
+
+exports.CreateAzureJwtToken = async (req, res) => {
+    try {
+        var jwtToken = await ApiSecurityAuthService.createAzureJwtToken(req.body.scope, req.body.username, req.body.password, req.body.clientId, req.body.tenantId);
+
+        return res.status(201).send(jwtToken);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+};
+
+exports.DeleteAzureJwtToken = async (req, res) => {
+    try {
+        await ApiSecurityAuthService.revokeRefreshToken(req.userId);
+
+        return res.status(200).send(null);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
--- server/ApiGenerator/CloudServices/Azure/ApiSecurityAuth/SecurityAuthService.js
+++ server/ApiGenerator/CloudServices/Azure/ApiSecurityAuth/SecurityAuthService.js
@@ -1,5 +1,8 @@
 const { DefaultAzureCredential } = require("@azure/identity");
 const { ApiManagementClient } = require("@azure/apimanagement");
+const { PublicClientApplication } = require("@azure/msal-node");
+const { GraphClient } = require("@microsoft/microsoft-graph-client");
+
 
 exports.createAzureApiKey = async (userId, productId, resourceGroup, serviceName, subscriptionId) => {
     const credential = new DefaultAzureCredential();
@@ -23,4 +26,33 @@ exports.deleteAzureApiKey = async (resourceGroup, serviceName, subscriptionId) =
     const client = new ApiManagementClient(credential, subscriptionId);
 
     await client.subscription.deleteMethod(resourceGroup, serviceName, subscriptionId);
+};
+
+exports.createAzureJwtToken = async (scope, username, password, clientId, tenantId) => {
+    const msalConfig = {
+        auth: {
+            clientId: clientId,
+            authority: `https://login.microsoftonline.com/${tenantId}`,
+        }
+    };
+
+    const pca = new PublicClientApplication(msalConfig);
+
+    const authParams = {
+        scopes: [scope], 
+        username: username, 
+        password: password 
+    };
+
+    const response = await pca.acquireTokenByUsernamePassword(authParams);
+
+    return response.accessToken;
+};
+
+exports.revokeRefreshToken = async (userId) => {
+    const credential = new DefaultAzureCredential();
+
+    const graphClient = GraphClient.initWithMiddleware({ authProvider: credential });
+
+    await graphClient.api(`/users/${userId}/revokeSignInSessions`).post();
 };
