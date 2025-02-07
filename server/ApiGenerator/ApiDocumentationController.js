e7cbdb414123d3397e9a07065194109139e9f9ca
Timothy Choi <tchoi720@gmail.com>
2025-02-06 19:47:17 -0800

    finish impl controller for doc service

--- server/ApiGenerator/ApiDocumentationController.js
+++ server/ApiGenerator/ApiDocumentationController.js
@@ -30,6 +30,56 @@ exports.CreateApiDocumentation = async (req, res) => {
     }
 };
 
+exports.EditApiDocumentationVersion = async (req, res) => {
+    try {
+       await ApiDocumentationService.editApiDocumentationVersion(req.documentationId, req.version);
+
+       return res.status(200).send(null);
+    } catch (error) {
+        throw new Error(error.message);
+    }
+};
+
+exports.EditApiDocumentationEndpoints = async (req, res) => {
+    try {
+       await ApiDocumentationService.setEndpoints(req.documentationId, req.body);
+
+       return res.status(200).send(null);
+    } catch (error) {
+        throw new Error(error.message);
+    }
+};
+
+exports.SetApiDocumentationAuth = async (req, res) => {
+    try {
+       await ApiDocumentationService.setAuthentication(req.documentationId, req.body);
+
+       return res.status(200).send(null);
+    } catch (error) {
+        throw new Error(error.message);
+    }
+};
+
+exports.EditApiDocumentationRateLimit = async (req, res) => {
+    try {
+       await ApiDocumentationService.setRateLimit(req.documentationId, req.rateLimit);
+
+       return res.status(200).send(null);
+    } catch (error) {
+        throw new Error(error.message);
+    }
+};
+
+exports.EditApiDocumentationInfo = async (req, res) => {
+    try {
+       await ApiDocumentationService.setDocumentationInfo(req.documentationId, req.body);
+
+       return res.status(200).send(null);
+    } catch (error) {
+        throw new Error(error.message);
+    }
+};
+
 exports.DeleteApiDocumentation = async (req, res) => {
     try {
         await ApiDocumentationService.deleteApiDocumentation(req.documentationId);
